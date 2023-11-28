251dcf14f62df807864efc6cbac3421f1aed3768
nick avlo <avlo@yahoo.com>
2023-11-28 12:02:55 -0800

    convenience order

--- src/main/java/com/prosilion/nostrrelay/config/WebSocketConfig.java
+++ src/main/java/com/prosilion/nostrrelay/config/WebSocketConfig.java
@@ -11,14 +11,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
-	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/topic");
-		config.setApplicationDestinationPrefixes("/app");
-	}
-
-	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/gs-guide-websocket");
 	}
 
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
 }

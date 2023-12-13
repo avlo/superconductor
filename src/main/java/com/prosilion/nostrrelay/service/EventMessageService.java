9082127a2e9b768567d7ba535cd7fa8f97cfb984
nick avlo <avlo@yahoo.com>
2023-12-13 13:17:05 -0800

    refactor

--- src/main/java/com/prosilion/nostrrelay/service/EventMessageService.java
+++ src/main/java/com/prosilion/nostrrelay/service/EventMessageService.java
@@ -2,14 +2,13 @@ package com.prosilion.nostrrelay.service;
 
 import lombok.extern.java.Log;
 import nostr.api.NIP01;
-import nostr.event.BaseMessage;
 import nostr.event.message.EventMessage;
 
 import java.util.logging.Level;
 
 @Log
-public class EventMessageService<T extends EventMessage> implements MessageService {
-  private final EventService eventService;
+public class EventMessageService<T extends EventMessage> implements MessageService<EventMessage> {
+  private final EventServiceImpl<EventMessageService<T>> eventService;
   private final T eventMessage;
 
   public EventMessageService(T eventMessage) {
@@ -18,11 +17,11 @@ public class EventMessageService<T extends EventMessage> implements MessageServi
     eventMessage.setSubscriptionId("SUB ID");
     log.log(Level.INFO, "EVENT message type: {0}", eventMessage.getEvent());
     this.eventMessage = eventMessage;
-    this.eventService = new EventServiceImpl(eventMessage);
+    this.eventService = new EventServiceImpl<>(eventMessage);
   }
 
   @Override
-  public BaseMessage processIncoming() {
+  public EventMessage processIncoming() {
     return NIP01.createEventMessage(eventService.processIncoming(), eventMessage.getSubscriptionId());
   }
 }

784727cd8cd78483974fd4802b008854217e4c0a
Nick Avlonitis <avlonitis.nick@mayo.edu>
2024-02-05 16:53:51 -0700

    debug logging

--- src/main/java/com/prosilion/nostrrelay/service/EventServiceImpl.java
+++ src/main/java/com/prosilion/nostrrelay/service/EventServiceImpl.java
@@ -1,9 +1,11 @@
 package com.prosilion.nostrrelay.service;
 
+import com.google.gson.Gson;
 import lombok.Getter;
 import lombok.extern.java.Log;
 import nostr.api.factory.impl.NIP01;
 import nostr.base.IEvent;
+import nostr.event.impl.GenericEvent;
 import nostr.event.message.EventMessage;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,10 +17,10 @@ public class EventServiceImpl<T extends EventMessage> implements EventService<T>
   private final T eventMessage;
   public EventServiceImpl(@NotNull T eventMessage) {
     this.eventMessage = eventMessage;
-//    log.log(Level.INFO, "EventService Constructed");
-//    log.log(Level.INFO, "EVENT message KIND: {0}", ((GenericEvent) eventMessage.getEvent()).getKind());
-//    log.log(Level.INFO, "EVENT message NIP: {0}", eventMessage.getNip());
-//    log.log(Level.INFO, "EVENT message JSON: {0}", event.toString());
+    log.log(Level.INFO, "EventService Constructed");
+    log.log(Level.INFO, "EVENT message KIND: {0}", ((GenericEvent) eventMessage.getEvent()).getKind());
+    log.log(Level.INFO, "EVENT message NIP: {0}", eventMessage.getNip());
+    log.log(Level.INFO, "EVENT message JSON: {0}", new Gson().toJson(eventMessage.getEvent().toString()));
   }
 
   public IEvent processIncoming() {

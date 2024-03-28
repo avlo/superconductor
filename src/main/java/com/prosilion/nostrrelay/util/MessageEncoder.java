7a7f6900fcbe9462ba4ed3671fd492a4713ebfc2
nick avlo <avlo@yahoo.com>
2024-03-28 15:24:51 -0700

    fix

--- src/main/java/com/prosilion/nostrrelay/util/MessageEncoder.java
+++ src/main/java/com/prosilion/nostrrelay/util/MessageEncoder.java
@@ -3,16 +3,15 @@ package com.prosilion.nostrrelay.util;
 import jakarta.websocket.Encoder;
 import nostr.event.BaseMessage;
 import nostr.event.json.codec.BaseMessageEncoder;
-import nostr.event.message.EventMessage;
 
-public class MessageEncoder<T extends BaseMessage> implements Encoder.Text<T> {
+public class MessageEncoder implements Encoder.Text<BaseMessage> {
 
   @Override
   public String encode(BaseMessage baseMessage) {
     return new BaseMessageEncoder(baseMessage, null).encode();
   }
 
-  public String encode(EventMessage eventMessage) {
-    return new BaseMessageEncoder(eventMessage, null).encode();
-  }
+  //  public String encode(EventMessage eventMessage) {
+  //    return new BaseMessageEncoder(eventMessage, null).encode();
+  //  }
 }

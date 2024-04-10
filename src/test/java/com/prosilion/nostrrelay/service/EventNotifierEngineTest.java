27a884d5152f356adce10b14bdd8e21c63e978ac
nick avlo <avlo@yahoo.com>
2024-04-10 16:31:12 -0700

    add classified tests

--- src/test/java/com/prosilion/nostrrelay/service/EventNotifierEngineTest.java
+++ src/test/java/com/prosilion/nostrrelay/service/EventNotifierEngineTest.java
@@ -2,11 +2,12 @@ package com.prosilion.nostrrelay.service;
 
 import com.prosilion.nostrrelay.pubsub.AddNostrEvent;
 import nostr.base.PublicKey;
-import nostr.event.BaseTag;
 import nostr.event.Kind;
 import nostr.event.impl.ClassifiedListingEvent;
+import nostr.event.impl.ClassifiedListingEvent.ClassifiedListing;
 import nostr.event.impl.TextNoteEvent;
 import nostr.event.tag.EventTag;
+import nostr.event.tag.PriceTag;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -17,75 +18,139 @@ import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 class EventNotifierEngineTest {
-  private static final PublicKey PUB_KEY_1 = new PublicKey("aaa73464e0688bb3f585f683e57fe1b95e1b47301172ccbe29b30a14ce358c70");
-  private static final PublicKey PUB_KEY_2 = new PublicKey("bbb73464e0688bb3f585f683e57fe1b95e1b47301172ccbe29b30a14ce358c70");
-  private static final PublicKey PUB_KEY_3 = new PublicKey("ccc73464e0688bb3f585f683e57fe1b95e1b47301172ccbe29b30a14ce358c70");
+  private static final PublicKey PUB_KEY_TEXTNOTE_1 = new PublicKey("aaa73464e0688bb3f585f683e57fe1b95e1b47301172ccbe29b30a14ce358c70");
+  private static final PublicKey PUB_KEY_TEXTNOTE_2 = new PublicKey("bbb73464e0688bb3f585f683e57fe1b95e1b47301172ccbe29b30a14ce358c70");
+  private static final PublicKey PUB_KEY_TEXTNOTE_3 = new PublicKey("ccc73464e0688bb3f585f683e57fe1b95e1b47301172ccbe29b30a14ce358c70");
+  private static final PublicKey PUB_KEY_CLASSIFIED_1 = new PublicKey("ddd73464e0688bb3f585f683e57fe1b95e1b47301172ccbe29b30a14ce358c70");
+  private static final PublicKey PUB_KEY_CLASSIFIED_2 = new PublicKey("eee73464e0688bb3f585f683e57fe1b95e1b47301172ccbe29b30a14ce358c70");
+  private static final PublicKey PUB_KEY_CLASSIFIED_3 = new PublicKey("fff73464e0688bb3f585f683e57fe1b95e1b47301172ccbe29b30a14ce358c70");
   private static final String CONTENT = "CONTENT";
-  private static TextNoteEvent textNoteEvent1;
-  private static TextNoteEvent textNoteEvent2;
-  private static ClassifiedListingEvent classifiedEvent1;
-  private static ClassifiedListingEvent classifiedEvent2;
-  private static List<BaseTag> baseTags;
   private static EventNotifierEngine eventNotifierEngine;
 
   @BeforeAll
   public static void setup() {
     eventNotifierEngine = new EventNotifierEngine();
-
-    baseTags = List.of(new EventTag("EVENT-1"));
-
-    textNoteEvent1 = new TextNoteEvent(
-        PUB_KEY_1,
-        baseTags,
-        CONTENT
-    );
-    textNoteEvent1.setId("1111111111");
-    textNoteEvent1.setKind(Kind.TEXT_NOTE.getValue());
-
-    textNoteEvent2 = new TextNoteEvent(
-        PUB_KEY_2,
-        baseTags,
-        CONTENT
-    );
-    textNoteEvent2.setId("2222222222");
-    textNoteEvent2.setKind(Kind.TEXT_NOTE.getValue());
   }
 
   @Test()
   @Order(2)
-  void addSingleEvent() {
-    textNoteEvent1.setId("333333333333333333");
+  void addSingleTextNoteEvent() {
+    TextNoteEvent textNoteEvent = new TextNoteEvent(
+        PUB_KEY_TEXTNOTE_3,
+        List.of(new EventTag("TEXT-NOTE-EVENT-3333")),
+        CONTENT
+    );
+
+    textNoteEvent.setId("333333333333333333");
     eventNotifierEngine.event(new AddNostrEvent<TextNoteEvent>(
         Kind.valueOf(
-            textNoteEvent1.getKind()
+            textNoteEvent.getKind()
         ),
-        Long.valueOf(textNoteEvent1.getId()),
-        textNoteEvent1));
+        Long.valueOf(textNoteEvent.getId()),
+        textNoteEvent)
+    );
   }
 
   @Test
   @Order(1)
   void addTwoTextNoteEvents() {
+    TextNoteEvent textNoteEvent1 = new TextNoteEvent(
+        PUB_KEY_TEXTNOTE_1,
+        List.of(new EventTag("TEXT-NOTE-EVENT-11111")),
+        CONTENT
+    );
+    textNoteEvent1.setId("1111111111");
+    textNoteEvent1.setKind(Kind.TEXT_NOTE.getValue());
+
     eventNotifierEngine.event(new AddNostrEvent<TextNoteEvent>(
         Kind.valueOf(
             textNoteEvent1.getKind()
         ),
         Long.valueOf(textNoteEvent1.getId()),
         textNoteEvent1));
+
+    TextNoteEvent textNoteEvent2 = new TextNoteEvent(
+        PUB_KEY_TEXTNOTE_2,
+        List.of(new EventTag("TEXT-NOTE-EVENT-2222")),
+        CONTENT
+    );
+    textNoteEvent2.setId("2222222222");
+    textNoteEvent2.setKind(Kind.TEXT_NOTE.getValue());
 
     eventNotifierEngine.event(new AddNostrEvent<TextNoteEvent>(
         Kind.valueOf(
             textNoteEvent2.getKind()
         ),
         Long.valueOf(textNoteEvent2.getId()),
-        textNoteEvent2));
+        textNoteEvent2)
+    );
   }
 
   @Test
-  void testEvent() {
+  @Order(4)
+  void testTwoClassifiedEvents() {
+    ClassifiedListingEvent classifiedEvent1 = new ClassifiedListingEvent(
+        PUB_KEY_CLASSIFIED_1,
+        List.of(new EventTag("CLASSIFIED-BASE-TAG-11111")),
+        CONTENT,
+        new ClassifiedListing(
+            "classified title 111111",
+            "classified summarysummary 11111",
+            new PriceTag("1111", "BTC", "1")
+        ));
+    classifiedEvent1.setId("1111111111");
+    classifiedEvent1.setKind(Kind.CLASSIFIED_LISTING.getValue());
+
+    eventNotifierEngine.event(new AddNostrEvent<ClassifiedListingEvent>(
+        Kind.valueOf(
+            classifiedEvent1.getKind()
+        ),
+        Long.valueOf(classifiedEvent1.getId()),
+        classifiedEvent1)
+    );
+
+    ClassifiedListingEvent classifiedEvent2 = new ClassifiedListingEvent(
+        PUB_KEY_CLASSIFIED_2,
+        List.of(new EventTag("CLASSIFIED-BASE-TAG-2222")),
+        CONTENT,
+        new ClassifiedListing(
+            "classified title 2222",
+            "classified summarysummary 22222",
+            new PriceTag("2222", "USD", "1")
+        ));
+    classifiedEvent2.setId("222222222222222");
+    classifiedEvent2.setKind(Kind.CLASSIFIED_LISTING.getValue());
+
+    eventNotifierEngine.event(new AddNostrEvent<ClassifiedListingEvent>(
+        Kind.valueOf(
+            classifiedEvent2.getKind()
+        ),
+        Long.valueOf(classifiedEvent2.getId()),
+        classifiedEvent2)
+    );
   }
 
   @Test
-  void testEvent1() {
+  @Order(3)
+  void addSingleClassifiedEvent() {
+    ClassifiedListingEvent classifiedEvent = new ClassifiedListingEvent(
+        PUB_KEY_CLASSIFIED_3,
+        List.of(new EventTag("CLASSIFIED-BASE-TAG-3333")),
+        CONTENT,
+        new ClassifiedListing(
+            "classified title 3333",
+            "classified summarysummary 3333",
+            new PriceTag("333", "USD", "1")
+        ));
+    classifiedEvent.setId("33333333333333");
+    classifiedEvent.setKind(Kind.CLASSIFIED_LISTING.getValue());
+
+    eventNotifierEngine.event(new AddNostrEvent<ClassifiedListingEvent>(
+        Kind.valueOf(
+            classifiedEvent.getKind()
+        ),
+        Long.valueOf(classifiedEvent.getId()),
+        classifiedEvent)
+    );
   }
 }

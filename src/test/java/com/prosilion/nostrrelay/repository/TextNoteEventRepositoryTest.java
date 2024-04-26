6e16e16c3a7acd17d7837a3cb45534b7b2304137
avlo <avlo@yahoo.com>
2024-04-25 20:07:13 -0700

    merged nostr-java lib updates

--- src/test/java/com/prosilion/nostrrelay/repository/TextNoteEventRepositoryTest.java
+++ src/test/java/com/prosilion/nostrrelay/repository/TextNoteEventRepositoryTest.java
@@ -5,7 +5,6 @@ import nostr.base.Signature;
 import nostr.event.BaseTag;
 import nostr.event.Kind;
 import nostr.event.impl.GenericTag;
-import nostr.id.IIdentity;
 import nostr.id.Identity;
 import nostr.util.NostrUtil;
 import org.junit.jupiter.api.Assertions;
@@ -22,54 +21,54 @@ import java.util.List;
 @DirtiesContext
 class TextNoteEventRepositoryTest {
 
-  @Autowired
-  EventEntityRepository repository;
-  EventDto eventDto;
+	@Autowired
+	EventEntityRepository repository;
+	EventDto eventDto;
 
-  @BeforeEach
-  void setUp() {
-    List<BaseTag> tags = List.of(
-        GenericTag.create("e", 1, "494001ac0c8af2a10f60f23538e5b35d3cdacb8e1cc956fe7a16dfa5cbfc4346"),
-        GenericTag.create("p", 1, "2bed79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984")
-    );
+	@BeforeEach
+	void setUp() {
+		List<BaseTag> tags = List.of(
+				GenericTag.create("e", 1, "494001ac0c8af2a10f60f23538e5b35d3cdacb8e1cc956fe7a16dfa5cbfc4346"),
+				GenericTag.create("p", 1, "2bed79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984")
+		);
 
-    byte[] rawData = NostrUtil.hexToBytes("123123");
-    Signature signature = new Signature();
-    signature.setRawData(rawData);
+		byte[] rawData = NostrUtil.hexToBytes("123123");
+		Signature signature = new Signature();
+		signature.setRawData(rawData);
 
-    IIdentity sender = Identity.getInstance();
-    eventDto = new EventDto(
-        sender.getPublicKey(),
-        "SOME ID",
-        Kind.TEXT_NOTE,
-        1,
-        12345667L,
-        signature,
-        tags,
-        "CONTENT");
-    eventDto.setId("ID");
-  }
+		Identity sender = Identity.generateRandomIdentity();
+		eventDto = new EventDto(
+				sender.getPublicKey(),
+				"SOME ID",
+				Kind.TEXT_NOTE,
+				1,
+				12345667L,
+				signature,
+				tags,
+				"CONTENT");
+		eventDto.setId("ID");
+	}
 
-  @Test
-  void saveEventTest() {
-    Assertions.assertDoesNotThrow(() -> repository.save(eventDto.convertDtoToEntity()));
-  }
+	@Test
+	void saveEventTest() {
+		Assertions.assertDoesNotThrow(() -> repository.save(eventDto.convertDtoToEntity()));
+	}
 
-  @Test
-  void convertReturnedEvenToDtoTest() throws InvocationTargetException, IllegalAccessException {
-    repository.save(eventDto.convertDtoToEntity());
-    Assertions.assertTrue(repository.findByContent("CONTENT").isPresent());
-  }
+	@Test
+	void convertReturnedEvenToDtoTest() throws InvocationTargetException, IllegalAccessException {
+		repository.save(eventDto.convertDtoToEntity());
+		Assertions.assertTrue(repository.findByContent("CONTENT").isPresent());
+	}
 
-  @Test
-  void notFoundEntityTest() throws InvocationTargetException, IllegalAccessException {
-    repository.save(eventDto.convertDtoToEntity());
-    Assertions.assertFalse(repository.findByContent("CONTENT_SHOULD_NOT_FIND").isPresent());
-  }
+	@Test
+	void notFoundEntityTest() throws InvocationTargetException, IllegalAccessException {
+		repository.save(eventDto.convertDtoToEntity());
+		Assertions.assertFalse(repository.findByContent("CONTENT_SHOULD_NOT_FIND").isPresent());
+	}
 
-  @Test
-  void entityToDtoConversionTest() throws InvocationTargetException, IllegalAccessException {
-    repository.save(eventDto.convertDtoToEntity());
-    Assertions.assertTrue(repository.findByContent("CONTENT").get().getContent().contains("CONTENT"));
-  }
+	@Test
+	void entityToDtoConversionTest() throws InvocationTargetException, IllegalAccessException {
+		repository.save(eventDto.convertDtoToEntity());
+		Assertions.assertTrue(repository.findByContent("CONTENT").get().getContent().contains("CONTENT"));
+	}
 }

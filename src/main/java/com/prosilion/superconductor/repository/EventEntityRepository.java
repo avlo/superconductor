ef7a29c9e6112d79b311eaba3745fa66c33e0f5d
nick avlo <avlo@yahoo.com>
2024-06-02 16:52:08 -0700

    ongoing

--- src/main/java/com/prosilion/superconductor/repository/EventEntityRepository.java
+++ src/main/java/com/prosilion/superconductor/repository/EventEntityRepository.java
@@ -4,9 +4,9 @@ import com.prosilion.superconductor.entity.EventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface EventEntityRepository extends JpaRepository<EventEntity, Long> {
-  Optional<EventEntity> findByContent(String content);
+  List<EventEntity> findByContent(String content);
 }

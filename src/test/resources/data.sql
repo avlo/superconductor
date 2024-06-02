d8353efcb5ddaf9a9c9c20dd25af2db3f169a8da
nick avlo <avlo@yahoo.com>
2024-06-02 12:59:58 -0700

    mysql added, working, continue with h2 for integration tests

---
+++ src/test/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO TESTDB.event (event_id, kind, nip, content, created_at, pub_key, signature) VALUES
+                       ("5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc"
+                       ,1
+                       ,1
+                       ,"1111111111"
+                       ,1717357053050
+                       ,"bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984"
+                       ,"86f25c161fec51b9e441bdb2c09095d5f8b92fdce66cb80d9ef09fad6ce53eaa14c5e16787c42f5404905536e43ebec0e463aee819378a4acbe412c533e60546")

577b4d9daf7e3fc9b7ec698a431ca9bba7165c1d
nick avlo <avlo@yahoo.com>
2024-06-08 15:52:28 -0700

    needed for id

--- src/main/java/com/prosilion/superconductor/entity/standard/EventTagEntity.java
+++ src/main/java/com/prosilion/superconductor/entity/standard/EventTagEntity.java
@@ -6,12 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.Setter;
 import nostr.event.BaseTag;
 import nostr.event.Marker;
 import nostr.event.tag.EventTag;
 
+@Setter
+@Getter
 @NoArgsConstructor
 @Entity
 @Table(name = "event_tag")

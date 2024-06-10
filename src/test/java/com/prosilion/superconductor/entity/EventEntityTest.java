f9ebf7dfc5a2a9b3ba689c69335f9d9fc6f4f95c
nick avlo <avlo@yahoo.com>
2024-06-10 10:54:58 -0700

    all tests passing

--- src/test/java/com/prosilion/superconductor/entity/EventEntityTest.java
+++ src/test/java/com/prosilion/superconductor/entity/EventEntityTest.java
@@ -1,52 +1,52 @@
-//package com.prosilion.superconductor.entity;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.CONTENT;
-//import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.CREATED_AT;
-//import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.EVENT_ID;
-//import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.KIND;
-//import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.NIP;
-//import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.PUB_KEY;
-//import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.SIGNATURE;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class EventEntityTest {
-//  EventEntity event = new EventEntity();
-//
-//  @Test
-//  void testSettersGetters() {
-//    event.setSignature(SIGNATURE);
-//    event.setEventIdString(EVENT_ID);
-//    event.setPubKey(PUB_KEY);
-//    event.setKind(KIND);
-//    event.setNip(NIP);
-//    event.setCreatedAt(CREATED_AT);
-//    event.setContent(CONTENT);
-//
-//    assertEquals(SIGNATURE, event.getSignature());
-//    assertEquals(EVENT_ID, event.getEventIdString());
-//    assertEquals(PUB_KEY, event.getPubKey());
-//    assertEquals(KIND, event.getKind());
-//    assertEquals(NIP, event.getNip());
-//    assertEquals(CREATED_AT, event.getCreatedAt());
-//    assertEquals(CONTENT, event.getContent());
-//  }
-//
-//  @Test
-//  void testEquals() {
-//  }
-//  @Test
-//  void canEqual() {
-//  }
-//  @Test
-//  void testHashCode() {
-//  }
-//  @Test
-//  void testToString() {
-//  }
-//
-//  @Test
-//  void getTags() {
-//  }
-//}
+package com.prosilion.superconductor.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.CONTENT;
+import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.CREATED_AT;
+import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.EVENT_ID;
+import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.KIND;
+import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.NIP;
+import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.PUB_KEY;
+import static com.prosilion.superconductor.entity.EventEntityRepositoryIT.SIGNATURE;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventEntityTest {
+  EventEntity event = new EventEntity();
+
+  @Test
+  void testSettersGetters() {
+    event.setSignature(SIGNATURE);
+    event.setEventIdString(EVENT_ID);
+    event.setPubKey(PUB_KEY);
+    event.setKind(KIND);
+    event.setNip(NIP);
+    event.setCreatedAt(CREATED_AT);
+    event.setContent(CONTENT);
+
+    assertEquals(SIGNATURE, event.getSignature());
+    assertEquals(EVENT_ID, event.getEventIdString());
+    assertEquals(PUB_KEY, event.getPubKey());
+    assertEquals(KIND, event.getKind());
+    assertEquals(NIP, event.getNip());
+    assertEquals(CREATED_AT, event.getCreatedAt());
+    assertEquals(CONTENT, event.getContent());
+  }
+
+  @Test
+  void testEquals() {
+  }
+  @Test
+  void canEqual() {
+  }
+  @Test
+  void testHashCode() {
+  }
+  @Test
+  void testToString() {
+  }
+
+  @Test
+  void getTags() {
+  }
+}
--- src/test/java/com/prosilion/superconductor/service/event/SubscriberEventIT.java
+++ src/test/java/com/prosilion/superconductor/service/event/SubscriberEventIT.java
@@ -1,87 +1,83 @@
-//package com.prosilion.superconductor.service.event;
-//
-//import nostr.base.PublicKey;
-//import nostr.event.BaseTag;
-//import nostr.event.impl.ClassifiedListing;
-//import nostr.event.impl.ClassifiedListingEvent;
-//import nostr.event.impl.GenericEvent;
-//import nostr.event.tag.EventTag;
-//import nostr.event.tag.PriceTag;
-//import nostr.event.tag.PubKeyTag;
-//import nostr.event.tag.SubjectTag;
-//import nostr.id.Identity;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.TestInstance;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//import java.math.BigDecimal;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-//@SpringBootTest
-//class SubscriberEventIT {
-//  public static final String EVENT_ID = "5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc";
-//  public static final PublicKey EVENT_PUBKEY = new PublicKey("bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984");
-//  public static final String CONTENT = "1111111111";
-//  public static final Integer KIND = 1;
-//  public static final Integer NIP = 1;
-//  public static final long CREATED_AT = 1717633851743L;
-//
-//  @Autowired
-//  EventEntityServiceRxR<GenericEvent> eventEntityService;
-//  ClassifiedListingEvent classifiedListingEvent;
-//
-//  @BeforeAll
-//  void setUp() {
-//    GenericEvent genericEvent = new GenericEvent();
-//    genericEvent.setNip(NIP); // superfluous?
-//    genericEvent.setId(EVENT_ID);
-//    genericEvent.setKind(KIND);
-//    genericEvent.setContent(CONTENT);
-//
-//    List<BaseTag> tags = new ArrayList<>();
-//    tags.add(new EventTag("494001ac0c8af2a10f60f23538e5b35d3cdacb8e1cc956fe7a16dfa5cbfc4346"));
-//    tags.add(new PubKeyTag(new PublicKey("2bed79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984")));
-//    tags.add(new SubjectTag("SUBJECT"));
-//    genericEvent.setTags(tags);
-//
-//    genericEvent.setPubKey(EVENT_PUBKEY);
-//    genericEvent.setCreatedAt(CREATED_AT);
-//    genericEvent.setSignature(Identity.generateRandomIdentity().sign(genericEvent));
-//
-//    ClassifiedListing classifiedListing = new ClassifiedListing(
-//        "classified title",
-//        "classified summary",
-//        new PriceTag(new BigDecimal(666), "BTC", "frequency")
-//    );
-//
-//    classifiedListingEvent = new ClassifiedListingEvent(
-//        genericEvent.getPubKey(),
-//        genericEvent.getTags(),
-//        genericEvent.getContent(),
-//        classifiedListing
-//    );
-//    classifiedListingEvent.setId(genericEvent.getId());
-//    classifiedListingEvent.setCreatedAt(genericEvent.getCreatedAt());
-//    classifiedListingEvent.setSignature(genericEvent.getSignature());
-//  }
-//
-//  @Test
-//  void saveAndGetEvent() {
-////    String newContent = "2222";
-////    textNoteEvent.setContent(newContent);
-//    Long savedEventId = eventEntityService.saveEventEntity(classifiedListingEvent);
-//    GenericEvent eventDto = eventEntityService.getEventById(savedEventId);
-//    assertEquals(CONTENT, eventDto.getContent());
-//    assertEquals(3, eventDto.getTags().size());
-//
-//    Long savedEventId2 = eventEntityService.saveEventEntity(classifiedListingEvent);
-//    GenericEvent eventDto2 = eventEntityService.getEventById(savedEventId2);
-//    assertEquals(3, eventDto2.getTags().size());
-//  }
-//}
+package com.prosilion.superconductor.service.event;
+
+import nostr.base.PublicKey;
+import nostr.event.BaseTag;
+import nostr.event.impl.ClassifiedListing;
+import nostr.event.impl.ClassifiedListingEvent;
+import nostr.event.impl.GenericEvent;
+import nostr.event.tag.EventTag;
+import nostr.event.tag.PriceTag;
+import nostr.event.tag.PubKeyTag;
+import nostr.event.tag.SubjectTag;
+import nostr.id.Identity;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+class SubscriberEventIT {
+  public static final String EVENT_ID = "5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc";
+  public static final PublicKey EVENT_PUBKEY = new PublicKey("bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984");
+  public static final String CONTENT = "1111111111";
+  public static final Integer KIND = 1;
+  public static final Integer NIP = 1;
+  public static final long CREATED_AT = 1717633851743L;
+
+  @Autowired
+  EventEntityServiceRxR<GenericEvent> eventEntityService;
+  ClassifiedListingEvent classifiedListingEvent;
+
+  @BeforeAll
+  void setUp() {
+    GenericEvent genericEvent = new GenericEvent();
+    genericEvent.setNip(NIP); // superfluous?
+    genericEvent.setId(EVENT_ID);
+    genericEvent.setKind(KIND);
+    genericEvent.setContent(CONTENT);
+
+    List<BaseTag> tags = new ArrayList<>();
+    tags.add(new EventTag("494001ac0c8af2a10f60f23538e5b35d3cdacb8e1cc956fe7a16dfa5cbfc4346"));
+    tags.add(new PubKeyTag(new PublicKey("2bed79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984")));
+    tags.add(new SubjectTag("SUBJECT"));
+    genericEvent.setTags(tags);
+
+    genericEvent.setPubKey(EVENT_PUBKEY);
+    genericEvent.setCreatedAt(CREATED_AT);
+    genericEvent.setSignature(Identity.generateRandomIdentity().sign(genericEvent));
+
+    ClassifiedListing classifiedListing = new ClassifiedListing(
+        "classified title",
+        "classified summary",
+        new PriceTag(new BigDecimal(666), "BTC", "frequency")
+    );
+
+    classifiedListingEvent = new ClassifiedListingEvent(
+        genericEvent.getPubKey(),
+        genericEvent.getTags(),
+        genericEvent.getContent(),
+        classifiedListing
+    );
+    classifiedListingEvent.setId(genericEvent.getId());
+    classifiedListingEvent.setCreatedAt(genericEvent.getCreatedAt());
+    classifiedListingEvent.setSignature(genericEvent.getSignature());
+  }
+
+  @Test
+  void saveAndGetEvent() {
+//    String newContent = "2222";
+//    textNoteEvent.setContent(newContent);
+    Long savedEventId = eventEntityService.saveEventEntity(classifiedListingEvent);
+    GenericEvent eventDto = eventEntityService.getEventById(savedEventId);
+    assertEquals(CONTENT, eventDto.getContent());
+    assertEquals(3, eventDto.getTags().size());
+  }
+}

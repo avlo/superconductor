fc68911f0c115aef39cd7ce4bb74a07c5081a48e
avlo <avlo@yahoo.com>
2024-06-13 12:26:13 -0700

    ongoing

--- src/main/java/com/prosilion/superconductor/dto/AbstractTagDto.java
+++ src/main/java/com/prosilion/superconductor/dto/AbstractTagDto.java
@@ -2,8 +2,8 @@ package com.prosilion.superconductor.dto;
 
 import com.prosilion.superconductor.entity.AbstractTagEntity;
 
-// TODO: refactor, may be unnecessary
 public interface AbstractTagDto {
   String getCode();
+
   AbstractTagEntity convertDtoToEntity();
 }
--- src/main/java/com/prosilion/superconductor/dto/standard/HashtagTagDto.java
+++ src/main/java/com/prosilion/superconductor/dto/standard/HashtagTagDto.java
@@ -2,11 +2,13 @@ package com.prosilion.superconductor.dto.standard;
 
 import com.prosilion.superconductor.dto.AbstractTagDto;
 import com.prosilion.superconductor.entity.standard.HashtagTagEntity;
+import lombok.NonNull;
 import nostr.event.tag.HashtagTag;
 
 public class HashtagTagDto implements AbstractTagDto {
   private final HashtagTag hashtagTag;
-  public HashtagTagDto(HashtagTag hashtagTag) {
+
+  public HashtagTagDto(@NonNull HashtagTag hashtagTag) {
     this.hashtagTag = hashtagTag;
   }
 

83cfa0dee1143b9836aef0f75d73950a531ca382
nick avlo <avlo@yahoo.com>
2024-06-13 09:49:28 -0700

    post refactor cleanup

--- src/main/java/com/prosilion/superconductor/repository/join/zap/ZapRequestEventEntityEventEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prosilion.superconductor.repository.join.zap;
-
-import com.prosilion.superconductor.entity.join.classified.ZapRequestEventEntityEventEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ZapRequestEventEntityEventEntityRepository extends JpaRepository<ZapRequestEventEntityEventEntity, Long> {
-}

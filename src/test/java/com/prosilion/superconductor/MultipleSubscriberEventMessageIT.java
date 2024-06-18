ec0601660ac74399876a26adf1e0c52121e5e1c2
nick avlo <avlo@yahoo.com>
2024-06-17 17:31:54 -0700

    multiples 100X looking good

--- src/test/java/com/prosilion/superconductor/MultipleSubscriberEventMessageIT.java
+++ src/test/java/com/prosilion/superconductor/MultipleSubscriberEventMessageIT.java
@@ -1,5 +1,7 @@
 package com.prosilion.superconductor;
 
+import kotlin.jvm.Synchronized;
+import lombok.Getter;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -14,7 +16,14 @@ import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.IntStream;
+
+import static org.awaitility.Awaitility.await;
 import static org.awaitility.Awaitility.given;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -22,7 +31,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 @DirtiesContext
-class EventMessageIT {
+class MultipleSubscriberEventMessageIT {
   public final static String globalEventJson = "[\"EVENT\",{\"id\":\"5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc\",\"kind\":1,\"content\":\"1111111111\",\"pubkey\":\"bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984\",\"created_at\":1717357053050,\"tags\":[],\"sig\":\"86f25c161fec51b9e441bdb2c09095d5f8b92fdce66cb80d9ef09fad6ce53eaa14c5e16787c42f5404905536e43ebec0e463aee819378a4acbe412c533e60546\"}]";
 
   private static final String TARGET_CONTENT = "5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc";
@@ -33,17 +42,27 @@ class EventMessageIT {
 
   private WebSocketStompClient eventStompClient;
 
+  private final Integer TARGET_COUNT = 100;
+  private final AtomicReference<Integer> resultCount = new AtomicReference<>(0);
+
+//  @Autowired
+//  private ServletContext servletContext;
+
   @BeforeAll
   public void setup() {
-    final WebSocketStompClient reqStompClient = new WebSocketStompClient(new StandardWebSocketClient());
-    reqStompClient.setMessageConverter(new MappingJackson2MessageConverter());
-
-    reqStompClient.getWebSocketClient().execute(new ReqMessageSocketHandler(), WEBSOCKET_URL, "");
-    reqStompClient.start();
+    IntStream.range(1, TARGET_COUNT).parallel().forEach(increment -> {
+      final WebSocketStompClient reqStompClient = new WebSocketStompClient(new StandardWebSocketClient());
+      reqStompClient.setMessageConverter(new MappingJackson2MessageConverter());
+      CompletableFuture<WebSocketSession> execute = reqStompClient.getWebSocketClient().execute(new ReqMessageSocketHandler(increment, generateRandomHexString()), WEBSOCKET_URL, "");
+      await().until(execute::isDone);
+      reqStompClient.start();
+    });
 
     eventStompClient = new WebSocketStompClient(new StandardWebSocketClient());
     eventStompClient.setMessageConverter(new MappingJackson2MessageConverter());
-    eventStompClient.getWebSocketClient().execute(new EventMessageSocketHandler(), WEBSOCKET_URL, "");
+    CompletableFuture<WebSocketSession> execute = eventStompClient.getWebSocketClient().execute(new EventMessageSocketHandler(), WEBSOCKET_URL, "");
+
+    await().until(execute::isDone);
   }
 
   @Test
@@ -51,8 +70,22 @@ class EventMessageIT {
     assertDoesNotThrow(() -> eventStompClient.start());
     assertDoesNotThrow(() -> eventStompClient.stop());
 
+//    ServletContext context = servletContext.getContext("/");
+//    Set<SessionTrackingMode> defaultSessionTrackingModes = servletContext.getDefaultSessionTrackingModes();
+//    Set<SessionTrackingMode> effectiveSessionTrackingModes = servletContext.getEffectiveSessionTrackingModes();
+//    Map<String, ? extends ServletRegistration> servletRegistrations = servletContext.getServletRegistrations();
+//    Enumeration<String> attributeNames = servletContext.getAttributeNames();
+
+    await().untilAtomic(resultCount, equalTo(TARGET_COUNT - 1));
+
     given().ignoreException(InterruptedException.class)
         .await().until(() -> !eventStompClient.isRunning());
+
+    System.out.println("-------------------");
+    System.out.println("-------------------");
+    System.out.printf("success count [%s] of target count [%s]%n", resultCount, TARGET_COUNT);
+    System.out.println("-------------------");
+    System.out.println("-------------------");
   }
 
   static class EventMessageSocketHandler extends TextWebSocketHandler {
@@ -68,9 +101,21 @@ class EventMessageIT {
     }
   }
 
+  @Synchronized
+  private void incrementRange() {
+    resultCount.getAndSet(resultCount.get() + 1);
+  }
+
+  @Getter
   class ReqMessageSocketHandler extends TextWebSocketHandler {
-    int count = 0;
-    private final static String reqJson = "[\"REQ\",\"16b8772f38bbdee735445862065f34adad5d1522b08203d02b1d1b207a798c9a\",{\"ids\":[\"5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc\"],\"authors\":[\"bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984\"]}]";
+    private final Integer index;
+    private boolean value = false;
+    private final String reqJson;
+
+    public ReqMessageSocketHandler(Integer index, String subId) {
+      this.index = index;
+      reqJson = "[\"REQ\",\"" + subId + "\",{\"ids\":[\"5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc\"],\"authors\":[\"bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984\"]}]";
+    }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
@@ -80,7 +125,20 @@ class EventMessageIT {
     @Override
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
       assertEquals(globalEventJson, message.getPayload().toString());
+      value = true;
+      incrementRange();
       session.close();
     }
+  }
+
+  private static String generateRandomHexString() {
+    Random random = new Random();
+    StringBuilder hexString = new StringBuilder();
+    for (int i = 0; i < 32; i++) {
+      int randomNumber = random.nextInt(16); // Generate a random number between 0 and 15 (inclusive)
+      char hexDigit = (randomNumber < 10) ? (char) ('0' + randomNumber) : (char) ('a' + (randomNumber - 10));
+      hexString.append(hexDigit);
+    }
+    return hexString.toString();
   }
 }

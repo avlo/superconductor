68014580986e302dacc1a768064954dda5128bc8
nick avlo <avlo@yahoo.com>
2024-06-19 16:29:47 -0700

    use string templates

--- src/test/java/com/prosilion/superconductor/MultipleSubscriberTextEventMessageIT.java
+++ src/test/java/com/prosilion/superconductor/MultipleSubscriberTextEventMessageIT.java
@@ -36,8 +36,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class MultipleSubscriberTextEventMessageIT {
   private static final String TARGET_TEXT_MESSAGE_EVENT_CONTENT = "5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc";
 
+  @SuppressWarnings("preview")
   public final static String textMessageEventJson =
-      "[\"EVENT\",{\"id\":\"" + TARGET_TEXT_MESSAGE_EVENT_CONTENT + "\",\"kind\":1,\"content\":\"1111111111\",\"pubkey\":\"bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984\",\"created_at\":1717357053050,\"tags\":[],\"sig\":\"86f25c161fec51b9e441bdb2c09095d5f8b92fdce66cb80d9ef09fad6ce53eaa14c5e16787c42f5404905536e43ebec0e463aee819378a4acbe412c533e60546\"}]";
+      StringTemplate.STR."""
+          [
+            "EVENT",
+              {
+                "id":"\{TARGET_TEXT_MESSAGE_EVENT_CONTENT}",
+                "kind":1,
+                "content":"1111111111",
+                "pubkey":"bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984",
+                "created_at":1717357053050,
+                "tags":
+                  [],
+                "sig":"86f25c161fec51b9e441bdb2c09095d5f8b92fdce66cb80d9ef09fad6ce53eaa14c5e16787c42f5404905536e43ebec0e463aee819378a4acbe412c533e60546"
+              }
+          ]
+      """;
 
   private static final String SCHEME_WS = "ws";
   private static final String HOST = "localhost";

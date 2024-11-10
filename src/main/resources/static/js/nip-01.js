5d90d221cdff4716cdafcc2e3ab85f788a22b500
nick avlo <avlo@yahoo.com>
2024-11-10 19:14:05 +0700

    refactored nip-01RxR replaces nip-01

--- src/main/resources/static/js/nip-01.js
+++ src/main/resources/static/js/nip-01.js
@@ -1,54 +1,43 @@
 $(function () {
-    $("#send01").click(() => hashThenSend01());
+    $("#send01").click(() => createEvent());
 });
 
-function hashThenSend01() {
-    const concat = [
-        '0',
-        pubKey,
-        $("#01-kind").val(),
-        $("#e_tag").val(),
-        $("#p_tag").val(),
-        $("#01-g_tag").val(),
-        $("#01-content").val()
-    ].join(",");
-
-    const text = [
-        '[',
-        concat,
-        ']'
-    ].join('');
-
-    createDigest(text).then((hash) => sendContent01(hash));
+function createEvent() {
+    var typeScriptEvent = generateTypeScriptEvent();
+    console.log("createEvent() created event JSON: \n\n" + typeScriptEvent + "\n\n");
+    signEvent(typeScriptEvent).then((fullyPopulatedSignedEvent) => sendContent01(fullyPopulatedSignedEvent));
 }
 
-function replaceHash01(id_hash) {
+function generateTypeScriptEvent() {
+    const tags = [
+        ['e', $("#e_tag").val()],
+        ['p', $("#p_tag").val()],
+        ['g', $("#01-g_tag").val()]
+    ];
+
+    let event = {
+        id: '',
+        kind: Number($("#01-kind").val()),
+        created_at: dateNow,
+        content: $("#01-content").val(),
+        tags: tags,
+        pubkey: '',
+        sig: ''
+    }
+    return event;
+}
+
+function sendContent01(signedPopulatedEvent) {
+    console.log("\nsending content...\n\n");
+    console.log(addEventMessageWrapper(signedPopulatedEvent));
+    console.log('\n\n');
+    ws.send(addEventMessageWrapper(signedPopulatedEvent));
+}
+
+function addEventMessageWrapper(id_hash) {
     return "["
         + "\"EVENT\","
-        + JSON.stringify(
-            {
-                'id': id_hash,
-                'kind': $("#01-kind").val(),
-                'content': $("#01-content").val(),
-                'tags': [
-                    ['e', $("#e_tag").val()],
-                    ['p', $("#p_tag").val()],
-                    ['g', $("#01-g_tag").val()]
-                ],
-                'pubkey': pubKey,
-                'created_at': Date.now(),
-                'sig': '86f25c161fec51b9e441bdb2c09095d5f8b92fdce66cb80d9ef09fad6ce53eaa14c5e16787c42f5404905536e43ebec0e463aee819378a4acbe412c533e60546'
-            }
+        + JSON.stringify(id_hash
         )
         + "]";
-}
-
-function sendContent01(id_hash) {
-    console.log("\nsending content...\n\n");
-    console.log("sending w/ date now: " + dateNow);
-    currentSubscriptonId = id_hash;
-    let localjsonstring = replaceHash01(id_hash);
-    console.log(localjsonstring);
-    console.log('\n\n');
-    ws.send(localjsonstring);
 }

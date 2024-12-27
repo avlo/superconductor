e539b9c5e90277a5f126b25b1490d5481adf6ba7
nick avlo <avlo@yahoo.com>
2024-12-27 18:48:17 -0500

    save working snapshot

---
+++ src/test/java/com/prosilion/superconductor/util/NostrRelayService.java
@@ -0,0 +1,199 @@
+package com.prosilion.superconductor.util;
+
+import com.google.common.collect.Streams;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import nostr.api.factory.impl.NIP01Impl.EventMessageFactory;
+import nostr.event.BaseMessage;
+import nostr.event.impl.GenericEvent;
+import nostr.event.json.codec.BaseEventEncoder;
+import nostr.event.json.codec.BaseMessageDecoder;
+import nostr.event.json.codec.GenericEventDecoder;
+import nostr.event.message.EventMessage;
+import nostr.event.message.OkMessage;
+import org.apache.logging.log4j.util.Strings;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+public class NostrRelayService {
+  private final StandardWebSocketClient eventSocketClient;
+  private Map<String, StandardWebSocketClient> requestSocketClientMap = new ConcurrentHashMap<>();
+  private final String relayUri;
+  private final String uuidPrefix;
+//  private final SslBundles sslBundles;
+
+  public NostrRelayService(
+      @Value("${superconductor.relay.url}") String relayUri,
+      @Value("${superconductor.test.uuid.prefix}") String uuidPrefix
+//      , SslBundles sslBundles
+  ) throws ExecutionException, InterruptedException {
+    this.relayUri = relayUri;
+    this.uuidPrefix = uuidPrefix;
+//    this.sslBundles = sslBundles;
+    this.eventSocketClient = new StandardWebSocketClient(relayUri
+//        , sslBundles
+    );
+  }
+
+  public void save(@NonNull String eventJson) throws IOException {
+    OkMessage okMessageClassifiedListing =
+        createNostrEvent(
+            eventJson
+        );
+
+    assertTrue(okMessageClassifiedListing.getFlag());
+  }
+
+  public void save(@NonNull GenericEvent event) throws IOException {
+    OkMessage okMessageClassifiedListing =
+        createNostrEvent(
+            new EventMessageFactory(event).create()
+        );
+
+    assertTrue(okMessageClassifiedListing.getFlag());
+  }
+
+  private OkMessage createNostrEvent(@NonNull String eventJson) throws IOException {
+    return getOkMessage(sendEvent(eventJson));
+  }
+
+  private OkMessage createNostrEvent(@NonNull EventMessage eventMessage) throws IOException {
+    return getOkMessage(sendEvent(eventMessage));
+  }
+
+  @NotNull
+  private static OkMessage getOkMessage(List<String> received) {
+    Optional<String> last = Streams.findLast(received.stream());
+    return last
+        .map(baseMessage -> new BaseMessageDecoder<OkMessage>().decode(baseMessage))
+        .orElseThrow();
+  }
+
+  private List<String> sendEvent(String eventJson) throws IOException {
+    eventSocketClient.send(eventJson);
+    log.info("socket send event JSON content\n  {}", eventJson);
+    return getEvents();
+  }
+
+  private List<String> sendEvent(EventMessage eventMessage) throws IOException {
+    eventSocketClient.send(eventMessage);
+    log.info("socket send EventMessage content\n  {}", eventMessage.getEvent());
+    return getEvents();
+  }
+
+  public List<String> getEvents() {
+    List<String> events = eventSocketClient.getEvents();
+    log.info("received relay response:");
+    log.info("\n" + events.stream().map(event -> String.format("  %s\n", event)).collect(Collectors.joining()));
+    return events;
+  }
+
+//  --------------------------------------------------------------------
+//  REQUESTS
+//  --------------------------------------------------------------------
+
+  public GenericEvent get(@NonNull String subscriberId) throws IOException, ExecutionException, InterruptedException {
+    GenericEvent event =
+        sendNostrRequest(
+            subscriberId,
+            GenericEvent.class
+        );
+
+    return event;
+  }
+
+  private List<String> sendRequest(@NonNull String key) throws ExecutionException, InterruptedException, IOException {
+    final StandardWebSocketClient webSocketClientIF = requestSocketClientMap.get(key);
+    if (webSocketClientIF != null) {
+      System.out.printf("3333333333333 existing REQ socket\nkey:\n  [%s]\nsocket:\n  [%s]\n\n", key, webSocketClientIF.getClientSession().getId());
+      List<String> events = webSocketClientIF.getEvents();
+      System.out.println("-------------");
+      System.out.println("socket getEvents():");
+      events.forEach(event -> System.out.printf("  %s\n", event));
+      System.out.println("33333333333\n");
+      return events;
+    }
+
+    requestSocketClientMap.put(key, new StandardWebSocketClient(relayUri
+//        , sslBundles
+    ));
+
+    final StandardWebSocketClient newWebSocketClientIF = requestSocketClientMap.get(key);
+    System.out.printf("222222222222 new REQ socket\nkey:\n  [%s]\nsocket:\n  [%s]\n\n", key, newWebSocketClientIF.getClientSession().getId());
+    newWebSocketClientIF.send(createReqJson(key, key));
+    List<String> events = newWebSocketClientIF.getEvents();
+    System.out.println("-------------");
+    System.out.println("socket getEvents():");
+    events.forEach(event -> System.out.printf("  %s\n", event));
+    System.out.println("222222222222\n");
+    return newWebSocketClientIF.getEvents();
+  }
+
+  private <T extends GenericEvent> T sendNostrRequest(
+      @NonNull String subscriberId,
+      @NonNull Class<T> type) throws IOException, ExecutionException, InterruptedException {
+    List<String> returnedEvents = sendRequest(
+        Strings.concat(uuidPrefix, subscriberId)
+    );
+
+    System.out.println("55555555555555555");
+    System.out.println("after REQUEST:");
+    System.out.printf("key:\n  [%s]\n", subscriberId);
+    System.out.println("-----------------");
+    System.out.println("returnedEvents:");
+    System.out.printf(returnedEvents.stream().map(event -> String.format("  %s\n", event)).collect(Collectors.joining()));
+    System.out.println("55555555555555555");
+    Stream<BaseMessage> baseMessageStream = returnedEvents.stream().map(baseMessage -> new BaseMessageDecoder<>().decode(baseMessage));
+
+    Stream<BaseMessage> baseMessageStream1 = baseMessageStream.filter(baseMessage -> !baseMessage.getCommand().equalsIgnoreCase("EOSE"));
+
+    Stream<BaseMessage> baseMessageStream2 = baseMessageStream1.filter(EventMessage.class::isInstance);
+
+    Stream<EventMessage> eventMessageStream = baseMessageStream2.map(EventMessage.class::cast);
+
+    Stream<GenericEvent> genericEventStream = eventMessageStream.map(eventMessage -> (GenericEvent) eventMessage.getEvent());
+
+    Stream<String> stringStream = genericEventStream.map(event -> new BaseEventEncoder<>(event).encode());
+
+    Stream<T> tStream = stringStream.map(encode -> new GenericEventDecoder<>(type).decode(encode));
+
+//    Optional<T> max = tStream.max((a, b) -> {
+//      System.out.println("a: " + a);
+//      System.out.println("b: " + b);
+//      return getCompare(a, b);
+//    });
+//    T latestDatedEvent = max.orElseThrow();
+
+    List<T> list = tStream.toList();
+    T last = list.getLast();
+    T latestDatedEvent = last;
+
+//    System.out.println("2222222222222222222");
+//    System.out.println("2222222222222222222");
+//    System.out.println(latestDatedEvent);
+//    System.out.println("2222222222222222222");
+//    System.out.println("2222222222222222222");
+    return latestDatedEvent;
+  }
+
+  private String createReqJson(@NonNull String subscriberId, @NonNull String dTag) {
+//    TODO: re-introduce below after investigation
+//    String result = ids.stream()
+//        .map(s -> "\"" + s + "\"")
+//        .collect(Collectors.joining(", "));
+//    String joinedString = "[\"REQ\",\"" + subscriberId + "\",{\"ids\":[" + result + "]}]";
+    return "[\"REQ\",\"" + subscriberId + "\",{\"ids\":[\"" + dTag + "\"]}]";
+  }
+}
---
+++ src/test/java/com/prosilion/superconductor/util/StandardWebSocketClient.java
@@ -0,0 +1,90 @@
+package com.prosilion.superconductor.util;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import nostr.event.BaseMessage;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHttpHeaders;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+
+import static org.awaitility.Awaitility.await;
+
+@Slf4j
+public class StandardWebSocketClient extends TextWebSocketHandler {
+  @Getter
+  private final WebSocketSession clientSession;
+  private final AtomicBoolean completed = new AtomicBoolean(false);
+
+  @Getter
+  private List<String> events = Collections.synchronizedList(new ArrayList<>());
+
+  public StandardWebSocketClient(@NonNull String relayUri
+//      , @NonNull SslBundles sslBundles
+  ) throws ExecutionException, InterruptedException {
+    org.springframework.web.socket.client.standard.StandardWebSocketClient standardWebSocketClient = new org.springframework.web.socket.client.standard.StandardWebSocketClient();
+//    standardWebSocketClient.setSslContext(sslBundles.getBundle("server").createSslContext());
+    this.clientSession = standardWebSocketClient
+        .execute(
+            this,
+            new WebSocketHttpHeaders(),
+            URI.create(relayUri))
+        .get();
+    log.info("WebSocket client connected {}", clientSession.getId());
+  }
+
+  @Override
+  protected void handleTextMessage(@NotNull WebSocketSession session, TextMessage message) {
+    String payload = message.getPayload();
+    System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
+    System.out.printf("socket:\n  [%s]\n", session.getId());
+    System.out.println("------------------------------");
+    System.out.println("  " + payload);
+    System.out.println("------------------------------");
+    System.out.println("events BEFORE payload:");
+    System.out.printf(events.stream().map(event -> String.format("  %s\n", event)).collect(Collectors.joining()));
+    System.out.println("------------------------------");
+    events.add(payload);
+    System.out.println("events AFTER  payload:");
+    System.out.printf(events.stream().map(event -> String.format("  %s\n", event)).collect(Collectors.joining()));
+    System.out.println("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ\n\n");
+    completed.setRelease(true);
+  }
+
+  public <T extends BaseMessage> void send(T eventMessage) throws IOException {
+    send(eventMessage.encode());
+  }
+
+  public void send(String json) throws IOException {
+    clientSession.sendMessage(new TextMessage(json));
+    await().untilTrue(completed);
+//    System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
+//    System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
+//    System.out.println("sending JSON:");
+//    System.out.println(json);
+
+////    System.out.println("------------------------------");
+////    List<String> eventList = Collections.synchronizedList(List.copyOf(events));
+////    events.clear();
+////    eventList.forEach(System.out::println);
+////    events = Collections.synchronizedList(new ArrayList<>());
+////    completed = false;
+
+//    System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
+//    System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYY\n\n\n\n\n\n\n\n");
+//    System.out.println("==============================");
+//    System.out.println("==============================\n\n\n\n\n\n\n\n");
+    completed.setRelease(false);
+  }
+}

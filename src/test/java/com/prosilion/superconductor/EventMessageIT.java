93ff99711eeab97a26296edffe3d424030020d00
nick avlo <avlo@yahoo.com>
2024-12-28 23:27:59 -0500

    ongoing test refactorings

--- src/test/java/com/prosilion/superconductor/EventMessageIT.java
+++ src/test/java/com/prosilion/superconductor/EventMessageIT.java
@@ -1,90 +1,113 @@
 package com.prosilion.superconductor;
 
+import com.prosilion.superconductor.util.NostrRelayService;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import nostr.base.Command;
+import org.apache.logging.log4j.util.Strings;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.test.context.ActiveProfiles;
 
-import static org.awaitility.Awaitility.given;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Slf4j
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
-@TestPropertySource("/application-test.properties")
 @DirtiesContext
+@ActiveProfiles("test")
 class EventMessageIT {
-  public final static String globalEventJson = "[\"EVENT\",\"16b8772f38bbdee735445862065f34adad5d1522b08203d02b1d1b207a798c9a\",{\"id\":\"5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc\",\"kind\":1,\"content\":\"1111111111\",\"pubkey\":\"bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984\",\"created_at\":1717357053050,\"tags\":[],\"sig\":\"86f25c161fec51b9e441bdb2c09095d5f8b92fdce66cb80d9ef09fad6ce53eaa14c5e16787c42f5404905536e43ebec0e463aee819378a4acbe412c533e60546\"}]";
+  public final static String globalEventJson = "[\"EVENT\",{\"id\":\"5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc\",\"kind\":1,\"content\":\"1111111111\",\"pubkey\":\"bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984\",\"created_at\":1717357053050,\"tags\":[],\"sig\":\"86f25c161fec51b9e441bdb2c09095d5f8b92fdce66cb80d9ef09fad6ce53eaa14c5e16787c42f5404905536e43ebec0e463aee819378a4acbe412c533e60546\"}]";
 
-  private static final String TARGET_CONTENT = "5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc";
-  private final String websocketUrl;
+  private final NostrRelayService nostrRelayService;
+  private final String uuidPrefix;
 
-  private WebSocketStompClient eventStompClient;
-
-  public EventMessageIT(@Value("${superconductor.relay.url}") String relayUrl) {
-    this.websocketUrl = relayUrl;
+  @Autowired
+  EventMessageIT(
+      @NonNull NostrRelayService nostrRelayService,
+      @Value("${superconductor.test.subscriberid.prefix}") String uuidPrefix
+  ) throws IOException {
+    this.nostrRelayService = nostrRelayService;
+    this.uuidPrefix = uuidPrefix;
   }
 
   @BeforeAll
-  public void setup() {
-    final WebSocketStompClient reqStompClient = new WebSocketStompClient(new StandardWebSocketClient());
-    reqStompClient.setMessageConverter(new MappingJackson2MessageConverter());
-
-    reqStompClient.getWebSocketClient().execute(new ReqMessageSocketHandler(), websocketUrl, "");
-    reqStompClient.start();
-
-    eventStompClient = new WebSocketStompClient(new StandardWebSocketClient());
-    eventStompClient.setMessageConverter(new MappingJackson2MessageConverter());
-    eventStompClient.getWebSocketClient().execute(new EventMessageSocketHandler(), websocketUrl, "");
+  public void setup() throws IOException {
+    log.debug("setup() send event:\n  {}", globalEventJson);
+    nostrRelayService.createEvent(globalEventJson);
+    assertTrue(!List.of(nostrRelayService.getEvents()).isEmpty());
   }
 
   @Test
-  void testEventMessageThenReqMessage() {
-    assertDoesNotThrow(() -> eventStompClient.start());
-    assertDoesNotThrow(() -> eventStompClient.stop());
+  void testReqFilteredByEventAndAuthor() throws IOException, ExecutionException, InterruptedException {
+    String uuidKey = "5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc";
+    String authorPubkey = "bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984";
 
-    given().ignoreException(InterruptedException.class)
-        .await().until(() -> !eventStompClient.isRunning());
+    Map<Command, Optional<String>> returnedJsonMap = nostrRelayService.sendRequest(
+        createReqJson(uuidKey, authorPubkey),
+        uuidKey
+    );
+    log.debug("okMessage:");
+    log.debug("  " + returnedJsonMap);
+    assertTrue(returnedJsonMap.get(Command.EVENT).get().contains(uuidKey));
+    assertTrue(returnedJsonMap.get(Command.EVENT).get().contains(authorPubkey));
   }
 
-  static class EventMessageSocketHandler extends TextWebSocketHandler {
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-      session.sendMessage(new TextMessage(globalEventJson));
-    }
-
-    @Override
-    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-      assertTrue(message.getPayload().toString().contains(TARGET_CONTENT));
-      session.close();
-    }
+  private String createReqJson(@NonNull String uuid, @NonNull String authorPubkey) {
+    final String uuidKey = Strings.concat(uuidPrefix, uuid);
+    return "[\"REQ\",\"" + uuidKey + "\",{\"ids\":[\"" + uuid + "\"],\"authors\":[\"" + authorPubkey + "\"]}]";
   }
 
-  class ReqMessageSocketHandler extends TextWebSocketHandler {
-    int count = 0;
-    private final static String reqJson = "[\"REQ\",\"16b8772f38bbdee735445862065f34adad5d1522b08203d02b1d1b207a798c9a\",{\"ids\":[\"5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc\"],\"authors\":[\"bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984\"]}]";
+  @Test
+  void testReqFilteredByEventId() throws IOException, ExecutionException, InterruptedException {
+    String uuidKey = "5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc";
 
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-      session.sendMessage(new TextMessage(reqJson));
-    }
+    Map<Command, Optional<String>> returnedJsonMap = nostrRelayService.sendRequest(
+        createEventReqJson(uuidKey),
+        uuidKey
+    );
+    log.debug("okMessage:");
+    log.debug("  " + returnedJsonMap);
+    assertTrue(returnedJsonMap.get(Command.EVENT).get().contains(uuidKey));
+  }
 
-    @Override
-    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-      assertEquals(globalEventJson, message.getPayload().toString());
-      session.close();
-    }
+  private String createEventReqJson(@NonNull String uuid) {
+    final String uuidKey = Strings.concat(uuidPrefix, uuid);
+    return "[\"REQ\",\"" + uuidKey + "\",{\"ids\":[\"" + uuid + "\"]}]";
+  }
+
+  @Test
+  void testReqFilteredByAuthor() throws IOException, ExecutionException, InterruptedException {
+    String authorPubkey = "bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984";
+
+    Map<Command, Optional<String>> returnedJsonMap = nostrRelayService.sendRequest(
+        createAuthorReqJson(authorPubkey),
+        authorPubkey
+    );
+    log.debug("okMessage:");
+    log.debug("  " + returnedJsonMap);
+
+    assertTrue(returnedJsonMap.get(Command.EVENT).get().contains(authorPubkey));
+
+//    additional eventId confirmation
+    String eventId = "5f66a36101d3d152c6270e18f5622d1f8bce4ac5da9ab62d7c3cc0006e5914cc";
+    assertTrue(returnedJsonMap.get(Command.EVENT).get().contains(eventId));
+  }
+
+  private String createAuthorReqJson(@NonNull String authorPubkey) {
+    final String uuidKey = Strings.concat(uuidPrefix, authorPubkey);
+    return "[\"REQ\",\"" + uuidKey + "\",{\"authors\":[\"" + authorPubkey + "\"]}]";
   }
 }

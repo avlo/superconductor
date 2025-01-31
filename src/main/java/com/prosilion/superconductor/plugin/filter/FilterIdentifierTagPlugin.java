a1ae5bb927f962f8aed6f6db7486691927a22e1a
nick avlo <avlo@yahoo.com>
2025-01-30 22:16:08 -0500

    generic tag refactor, use as template for remaining generic tag plugins

--- src/main/java/com/prosilion/superconductor/plugin/filter/FilterIdentifierTagPlugin.java
+++ src/main/java/com/prosilion/superconductor/plugin/filter/FilterIdentifierTagPlugin.java
@@ -1,5 +1,6 @@
 package com.prosilion.superconductor.plugin.filter;
 
+import com.prosilion.superconductor.plugin.filter.FilterIdentifierTagPlugin.IdentifierTagWrapper;
 import com.prosilion.superconductor.service.request.pubsub.AddNostrEvent;
 import nostr.event.BaseTag;
 import nostr.event.impl.Filters;
@@ -7,30 +8,34 @@ import nostr.event.impl.GenericEvent;
 import nostr.event.tag.IdentifierTag;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
 
 @Component
-public class FilterIdentifierTagPlugin<T extends Map<String, List<String>>> implements FilterPlugin<T> {
+public class FilterIdentifierTagPlugin<T extends IdentifierTagWrapper> implements FilterPlugin<T> {
 
   @Override
   public BiPredicate<T, AddNostrEvent<GenericEvent>> getBiPredicate() {
     return (t, u) ->
         getIdentifierTags(u.event().getTags()).stream()
             .map(IdentifierTag::getId)
-            .anyMatch(dTag -> t.entrySet().stream().anyMatch(entry -> entry.getValue().getFirst().equals(dTag)));
+            .anyMatch(dTag ->
+                t.identifierTags().entrySet().stream().anyMatch(entry ->
+                    entry.getValue().stream().anyMatch(value -> value.equals(dTag))));
+
   }
 
   @Override
-  public List<T> getPluginFilters(Filters filters) {
-    return (List<T>) List.of(
-        Optional.ofNullable(
-                filters.getGenericTagQuery())
-            .orElse(
-                Collections.emptyMap()));
+  public Optional<List<T>> getPluginFilters(Filters filters) {
+    return Optional.of((List<T>) List.of(new IdentifierTagWrapper(
+        filters.getGenericTagQuery().entrySet().stream()
+            .filter(mapEntry ->
+                mapEntry.getKey().equals("#d"))
+            .collect(Collectors.toMap(Entry::getKey, Entry::getValue)))));
   }
 
   @Override
@@ -43,5 +48,8 @@ public class FilterIdentifierTagPlugin<T extends Map<String, List<String>>> impl
         .filter(IdentifierTag.class::isInstance)
         .map(IdentifierTag.class::cast)
         .toList();
+  }
+
+  record IdentifierTagWrapper(Map<String, List<String>> identifierTags) {
   }
 }

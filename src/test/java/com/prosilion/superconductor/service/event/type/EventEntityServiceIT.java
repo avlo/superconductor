be9301d22fd5d6a116f73b39b8714adfa6cc0232
nick avlo <avlo@yahoo.com>
2025-03-08 15:15:10 -0800

    tests refactor

--- src/test/java/com/prosilion/superconductor/service/event/type/EventEntityServiceIT.java
+++ src/test/java/com/prosilion/superconductor/service/event/type/EventEntityServiceIT.java
@@ -1,24 +1,17 @@
 package com.prosilion.superconductor.service.event.type;
 
+import com.prosilion.superconductor.util.Factory;
 import nostr.base.PublicKey;
 import nostr.event.BaseTag;
+import nostr.event.Kind;
 import nostr.event.impl.GenericEvent;
-import nostr.event.impl.TextNoteEvent;
-import nostr.event.tag.EventTag;
-import nostr.event.tag.GeohashTag;
-import nostr.event.tag.HashtagTag;
-import nostr.event.tag.PriceTag;
-import nostr.event.tag.PubKeyTag;
-import nostr.event.tag.SubjectTag;
+import nostr.event.tag.*;
 import nostr.id.Identity;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,37 +19,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@DirtiesContext
 class EventEntityServiceIT {
-  public static final PublicKey EVENT_PUBKEY = new PublicKey("bbbd79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984");
-  public static final PubKeyTag P_TAG = new PubKeyTag(new PublicKey("2bed79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76985"));
-  public static final EventTag E_TAG = new EventTag("494001ac0c8af2a10f60f23538e5b35d3cdacb8e1cc956fe7a16dfa5cbfc4347");
+  private static final Identity IDENTITY = Factory.createNewIdentity();
+  private static final PublicKey EVENT_PUBKEY = IDENTITY.getPublicKey();
+  private static final PubKeyTag P_TAG = Factory.createPubKeyTag(IDENTITY);
 
-  public static final SubjectTag SUBJECT_TAG = new SubjectTag("Test Subject Tag");
-  public static final GeohashTag G_TAG = new GeohashTag("Test Geohash Tag");
-  public static final HashtagTag T_TAG = new HashtagTag("Test Hashtag Tag");
+  private static final EventTag E_TAG = Factory.createEventTag(EventEntityServiceIT.class);
+  private static final GeohashTag G_TAG = Factory.createGeohashTag(EventEntityServiceIT.class);
+  private static final HashtagTag T_TAG = Factory.createHashtagTag(EventEntityServiceIT.class);
+  private static final SubjectTag SUBJECT_TAG = Factory.createSubjectTag(EventEntityServiceIT.class);
+  private static final PriceTag PRICE_TAG = Factory.createPriceTag();
 
-  public static final BigDecimal NUMBER = new BigDecimal("2.71");
-  public static final String CURRENCY = "BTC";
-  public static final String FREQUENCY = "NANOSECOND";
-  public static final PriceTag PRICE_TAG = new PriceTag(NUMBER, CURRENCY, FREQUENCY);
-
-  public static final String CONTENT = "Test Content";
-  public static final Integer KIND = 1;
+  private final static String CONTENT = Factory.lorumIpsum(EventEntityServiceIT.class);
+  private final static int KIND = Kind.TEXT_NOTE.getValue();
 
   @Autowired
   EventEntityService<GenericEvent> eventEntityService;
 
-  TextNoteEvent textNoteEvent;
+  private static GenericEvent textNoteEvent;
 
   @BeforeAll
-  void setUp() {
-    GenericEvent genericEvent = new GenericEvent();
-
-    genericEvent.setKind(KIND);
-    genericEvent.setContent(CONTENT);
-
+  static void setUp() {
     List<BaseTag> tags = new ArrayList<>();
     tags.add(E_TAG);
     tags.add(P_TAG);
@@ -64,25 +47,22 @@ class EventEntityServiceIT {
     tags.add(G_TAG);
     tags.add(T_TAG);
     tags.add(PRICE_TAG);
-    genericEvent.setTags(tags);
 
-    genericEvent.setPubKey(EVENT_PUBKEY);
-    genericEvent.setSignature(Identity.generateRandomIdentity().sign(genericEvent));
-
-    textNoteEvent = new TextNoteEvent(
-        genericEvent.getPubKey(),
-        genericEvent.getTags(),
-        genericEvent.getContent()
-    );
-    textNoteEvent.setId(genericEvent.getId());
-    textNoteEvent.setCreatedAt(genericEvent.getCreatedAt());
-    textNoteEvent.setSignature(genericEvent.getSignature());
+    textNoteEvent = Factory.createTextNoteEvent(IDENTITY, tags, CONTENT);
+    IDENTITY.sign(textNoteEvent);
+    System.out.println("textNoteEvent getPubKey().toString(): " + textNoteEvent.getPubKey().toString());
+    System.out.println("textNoteEvent getPubKey().toHexString(): " + textNoteEvent.getPubKey().toHexString());
+    System.out.println("textNoteEvent getPubKey().toBech32String(): " + textNoteEvent.getPubKey().toBech32String());
   }
 
   @Test
   void saveAndGetEventWithGeohash() {
     Long savedEventId = eventEntityService.saveEventEntity(textNoteEvent);
     GenericEvent savedEvent = eventEntityService.getEventById(savedEventId);
+
+    System.out.println("savedEvent getPubKey().toString(): " + savedEvent.getPubKey().toString());
+    System.out.println("savedEvent getPubKey().toHexString(): " + savedEvent.getPubKey().toHexString());
+    System.out.println("savedEvent getPubKey().toBech32String(): " + savedEvent.getPubKey().toBech32String());
 
     assertEquals(CONTENT, savedEvent.getContent());
     assertEquals(KIND, savedEvent.getKind());
@@ -121,23 +101,23 @@ class EventEntityServiceIT {
         .map(HashtagTag.class::cast)
         .map(HashtagTag::toString).findFirst().orElseThrow());
 
-    assertEquals(NUMBER, PRICE_TAG.getNumber());
-    assertEquals(CURRENCY, PRICE_TAG.getCurrency());
-    assertEquals(FREQUENCY, PRICE_TAG.getFrequency());
+    assertEquals(PRICE_TAG.getNumber(), PRICE_TAG.getNumber());
+    assertEquals(PRICE_TAG.getCurrency(), PRICE_TAG.getCurrency());
+    assertEquals(PRICE_TAG.getFrequency(), PRICE_TAG.getFrequency());
 
-    assertEquals(NUMBER, savedEventTags.stream().filter(baseTag ->
+    assertEquals(PRICE_TAG.getNumber(), savedEventTags.stream().filter(baseTag ->
             baseTag.getCode().equalsIgnoreCase("price"))
         .filter(PriceTag.class::isInstance)
         .map(PriceTag.class::cast)
         .map(PriceTag::getNumber).findFirst().orElseThrow());
 
-    assertEquals(CURRENCY, savedEventTags.stream().filter(baseTag ->
+    assertEquals(PRICE_TAG.getCurrency(), savedEventTags.stream().filter(baseTag ->
             baseTag.getCode().equalsIgnoreCase("price"))
         .filter(PriceTag.class::isInstance)
         .map(PriceTag.class::cast)
         .map(PriceTag::getCurrency).findFirst().orElseThrow());
 
-    assertEquals(FREQUENCY, savedEventTags.stream().filter(baseTag ->
+    assertEquals(PRICE_TAG.getFrequency(), savedEventTags.stream().filter(baseTag ->
             baseTag.getCode().equalsIgnoreCase("price"))
         .filter(PriceTag.class::isInstance)
         .map(PriceTag.class::cast)
---
+++ src/test/java/com/prosilion/superconductor/util/Factory.java
@@ -0,0 +1,110 @@
+package com.prosilion.superconductor.util;
+
+import lombok.Getter;
+import nostr.api.factory.impl.NIP01Impl;
+import nostr.event.BaseTag;
+import nostr.event.impl.GenericEvent;
+import nostr.event.impl.TextNoteEvent;
+import nostr.event.tag.*;
+import nostr.id.Identity;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+public class Factory {
+
+  public static Identity createNewIdentity() {
+    return Identity.generateRandomIdentity();
+  }
+
+  public static <T extends GenericEvent> T createTextNoteEvent(Identity identity, List<BaseTag> tags, String content) {
+    TextNoteEvent textNoteEvent = new NIP01Impl.TextNoteEventFactory(identity, tags, content).create();
+
+//    NIP01<NIP01Event> nip01_1 = new NIP01<>(identity);
+//    EventNostr sign = nip01_1.createTextNoteEvent(tags, content).sign();
+//    return sign;
+
+    return (T) textNoteEvent;
+  }
+
+  public static GenericEvent createGenericEvent() {
+    String concat = UUID.randomUUID().toString().concat(UUID.randomUUID().toString()).replace("-", "");
+    return new GenericEvent(concat.substring(0, 64));
+  }
+
+  public static <T> SubjectTag createSubjectTag(Class<T> clazz) {
+    return new SubjectTag(clazz.getName() + " Subject Tag");
+  }
+
+  public static PubKeyTag createPubKeyTag(Identity identity) {
+    return new PubKeyTag(identity.getPublicKey());
+  }
+
+  public static <T> GeohashTag createGeohashTag(Class<T> clazz) {
+    return new GeohashTag(clazz.getName() + " Geohash Tag");
+  }
+
+  public static <T> HashtagTag createHashtagTag(Class<T> clazz) {
+    return new HashtagTag(clazz.getName() + " Hashtag Tag");
+  }
+
+  public static <T> EventTag createEventTag(Class<T> clazz) {
+    return new EventTag(createGenericEvent().getId());
+  }
+
+  public static PriceTag createPriceTag() {
+    Factory.PriceComposite pc = new Factory.PriceComposite();
+    BigDecimal NUMBER = pc.getPrice();
+    String CURRENCY = pc.getCurrency();
+    String FREQUENCY = pc.getFrequency();
+    return new PriceTag(NUMBER, CURRENCY, FREQUENCY);
+  }
+
+  public static <T> String lorumIpsumApache(Class<T> clazz) {
+    int length = 10;
+    boolean useLetters = true;
+    boolean useNumbers = false;
+    return RandomStringUtils.random(length, useLetters, useNumbers);
+  }
+
+  public static <T> String lorumIpsum() {
+    return lorumIpsum(Factory.class);
+  }
+
+  public static <T> String lorumIpsum(Class<T> clazz) {
+    return lorumIpsum(clazz, 10);
+  }
+
+  public static <T> String lorumIpsum(Class<T> clazz, int length) {
+    return lorumIpsum(clazz.getSimpleName(), length);
+  }
+
+  public static <T> String lorumIpsum(String s, int length) {
+    byte[] byteArray = new byte[length];
+    new Random().nextBytes(byteArray);
+    return s + new String(byteArray, StandardCharsets.UTF_8);
+  }
+
+  public static BigDecimal createRandomBigDecimal() {
+    Random rand = new Random();
+    int max = 100, min = 50;
+    int i = rand.nextInt(max - min + 1) + min;
+    int j = (rand.nextInt(max - min + 1) + min);
+    return new BigDecimal(String.valueOf(i) + '.' + j);
+  }
+
+  @Getter
+  private static class PriceComposite {
+    private final String currency = "BTC";
+    private final String frequency = "nanosecond";
+    private final BigDecimal price;
+
+    private PriceComposite() {
+      price = createRandomBigDecimal();
+    }
+  }
+}

200701805d8bc8d43c3d4623f7855a4bcd5c9cca
nick avlo <avlo@yahoo.com>
2025-03-22 23:31:43 -0700

    add maven option to autotest

--- autotest.sh
+++ autotest.sh
@@ -10,6 +10,37 @@ NOSTR_HOME=$GIT_HOME/avlo-nostr-java-fork/
 SUPER_HOME=$GIT_HOME/superconductor/
 NOSTR_JAVA_MAVEN_LOCAL_REPO=$M2_HOME/xyz/tcheeric
 
+# build tool mode defaults to gradle
+MODE="gradle"
+
+invoke_builder() {
+  if [ $MODE == "gradle" ]; then
+    gradle clean build -x test; return
+  fi
+  mvn clean -Dmaven.test.skip=true    
+}
+
+invoke_publisher() {
+  if [ $MODE == "gradle" ]; then
+    gradle publishToMavenLocal; return
+  fi
+  mvn install    
+}
+
+invoke_runner() {
+  if [ $MODE == "gradle" ]; then
+    gradle bootRunLocalWs; return
+  fi
+  mvn spring-boot:run -P local_ws    
+}
+
+invoke_tester() {
+  if [ $MODE == "gradle" ]; then
+      gradle test --rerun-tasks; return
+  fi
+  mvn verify
+}
+
 banner() {
   echo "+-------------------------------------------------------------------+"
   printf "| %-65s |\n" "$(date)"
@@ -28,28 +59,28 @@ content_line() {
 
 cd_nostr() {
   banner "cd to nostr-java:" "" "     [$NOSTR_HOME]"
-  cd $NOSTR_HOME || exit 
+  cd $NOSTR_HOME || (exit 33) 
 }
 
 cd_superconductor() {
   banner "cd to superconductor:" "" "     [$SUPER_HOME]"
-  cd $SUPER_HOME || exit 
+  cd $SUPER_HOME || (exit 33) 
 }
 
 rm_maven_local() {
   banner "clear nostr-java [$NOSTR_JAVA_MAVEN_LOCAL_REPO] repo"
-  rm -rf $NOSTR_JAVA_MAVEN_LOCAL_REPO || exit   
+  rm -rf $NOSTR_JAVA_MAVEN_LOCAL_REPO || (exit 33)   
 }
 
 build_nostr() {
   banner "building nostr-java..."
-  gradle clean build -x test || exit
+  invoke_builder || (exit 33)
   banner "...completed nostr-java build" 
 }
 
 publish_nostr_to_m2_local() {
   banner "publishing to m2/local local:" "" "       [$NOSTR_JAVA_MAVEN_LOCAL_REPO]"  
-  gradle publishToMavenLocal || exit
+  invoke_publisher || (exit 33)
   banner "...completed publishing to m2/local"
   banner "$NOSTR_JAVA_MAVEN_LOCAL_REPO contents:"
   tree -D $NOSTR_JAVA_MAVEN_LOCAL_REPO
@@ -58,7 +89,7 @@ publish_nostr_to_m2_local() {
 build_and_test_super() {
   banner "building superconductor & running tests..."
   sleep .01
-  gradle clean build --rerun-tasks || return 
+  invoke_tester || return 
   banner "...completed superconductor build and test"
   sleep .01  
 }
@@ -93,7 +124,7 @@ killshell() {
 
 run_super_service() {
   cd_superconductor
-  { gradle bootRunLocalWs & } || exit 
+  { invoke_runner & } || (exit 33) 
   SUPER_PID=$!
   banner "starting superconductor service pid: [$SUPER_PID]" "& waiting [$SLEEP] seconds prior to starting nostr-java test"
   sleep $SLEEP
@@ -102,28 +133,50 @@ run_super_service() {
 run_nostr_tests() {
   banner "...[$SLEEP] seconds wait over, starting nostr-java tests..."
   cd_nostr
-  { gradle test --rerun-tasks & } || exit
+  { invoke_tester & } || (exit 33)
   NOSTR_PID=$!
   banner "...nostr-java tests started, pid: [$NOSTR_PID]..."
   wait $NOSTR_PID
   banner "...nostr-java tests done"
 }
 
+user_prompt() {
+  while true; do
+      read -p "Y/y or enter for Gradle, M/m for Maven)" yesno
+      case $yesno in
+          [Yy]* ) 
+              echo "Gradle selected"
+              return
+          ;;
+          [Mm]* ) 
+              echo "Maven selected"
+              MODE="maven"
+              return
+          ;;
+          * ) echo "Default (Gradle) selected"
+              return
+          ;;
+      esac
+  done
+}
+
 ########################################
 ###########     main    ################
 ########################################
 
+user_prompt 
+
 { cd_nostr; rm_maven_local; build_nostr; publish_nostr_to_m2_local; } 
 banner "nostr-java dependencies completed"
-
 cd_superconductor
+
 build_and_test_super
 super_completion_code=$?
 banner "super exit code [$super_completion_code]"
 if [ $super_completion_code != 0 ]; then
   banner "erroneous exit, tests not performed"
   terminate_super
-  exit;
+  (exit 33);
 fi
 
 banner "superconductor dependencies completed"

32af105d0f781ee3089374f1fc3152fae5bf61e7
nick avlo <avlo@yahoo.com>
2025-04-21 17:07:57 -0700

    client update

--- app/src/test/java/com/prosilion/superconductor/util/NostrRelayService.java
+++ app/src/test/java/com/prosilion/superconductor/util/NostrRelayService.java
@@ -1,6 +1,7 @@
 package com.prosilion.superconductor.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.prosilion.subdivisions.service.NostrRelayClient;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -19,24 +20,21 @@ import org.springframework.boot.ssl.SslBundles;
 
 @Slf4j
 public class NostrRelayService {
-  private final com.prosilion.subdivisions.service.NostrRelayService nostrRelayService;
+  private final NostrRelayClient nostrRelayService;
 
-  public NostrRelayService(@Value("${superconductor.relay.url}") String relayUri) throws ExecutionException, InterruptedException {
+  public NostrRelayService(@Value("${superconductor.relay.url}") @NonNull String relayUri) throws ExecutionException, InterruptedException {
     log.debug("relayUri: \n{}", relayUri);
-    this.nostrRelayService = new com.prosilion.subdivisions.service.NostrRelayService(relayUri);
+    this.nostrRelayService = new NostrRelayClient(relayUri);
   }
 
-  public NostrRelayService(
-      @Value("${superconductor.relay.url}") String relayUri,
-      SslBundles sslBundles
-  ) throws ExecutionException, InterruptedException {
+  public NostrRelayService(@Value("${superconductor.relay.url}") @NonNull String relayUri, @NonNull SslBundles sslBundles) throws ExecutionException, InterruptedException {
     log.debug("relayUri: \n{}", relayUri);
     log.debug("sslBundles: \n{}", sslBundles);
     final SslBundle server = sslBundles.getBundle("server");
     log.debug("sslBundles name: \n{}", server);
     log.debug("sslBundles key: \n{}", server.getKey());
     log.debug("sslBundles protocol: \n{}", server.getProtocol());
-    this.nostrRelayService = new com.prosilion.subdivisions.service.NostrRelayService(relayUri);
+    this.nostrRelayService = new NostrRelayClient(relayUri, sslBundles);
   }
 
   public OkMessage sendEvent(@NonNull String eventJson) throws IOException {

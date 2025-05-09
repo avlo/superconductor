4089afd7cc7ee996e30f2957f231f122f931d61f
nick avlo <avlo@yahoo.com>
2025-05-09 16:54:42 -0700

    add unique eventIdString constraint on save()

--- lib/src/main/java/com/prosilion/superconductor/entity/EventEntity.java
+++ lib/src/main/java/com/prosilion/superconductor/entity/EventEntity.java
@@ -1,6 +1,7 @@
 package com.prosilion.superconductor.entity;
 
 import com.prosilion.superconductor.dto.EventDto;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -30,12 +31,14 @@ public class EventEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  private String signature;
+  @Column(unique = true)
   private String eventIdString;
+
   private String pubKey;
   private Integer kind;
   private Integer nip;
   private Long createdAt;
+  private String signature;
 
   @Lob
   private String content;

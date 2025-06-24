b93cec15f265e823718eac90255e1f944cc80124
nick avlo <avlo@yahoo.com>
2025-06-24 15:35:27 -0700

    add ReqServiceConfig autoconfiguration with ReqServiceIF as @Bean

--- lib/src/main/java/com/prosilion/superconductor/service/request/ReqService.java
+++ lib/src/main/java/com/prosilion/superconductor/service/request/ReqService.java
@@ -3,17 +3,13 @@ package com.prosilion.superconductor.service.request;
 import com.prosilion.nostr.message.ReqMessage;
 import com.prosilion.superconductor.entity.Subscriber;
 import com.prosilion.superconductor.util.EmptyFiltersException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
-import org.springframework.stereotype.Service;
 
-@Service
 public class ReqService implements ReqServiceIF {
   private final AbstractSubscriberService abstractSubscriberService;
   private final NotifierService notifierService;
 
-  @Autowired
-  public ReqService(AbstractSubscriberService abstractSubscriberService, NotifierService notifierService) {
+  public ReqService(@NonNull AbstractSubscriberService abstractSubscriberService, @NonNull NotifierService notifierService) {
     this.abstractSubscriberService = abstractSubscriberService;
     this.notifierService = notifierService;
   }

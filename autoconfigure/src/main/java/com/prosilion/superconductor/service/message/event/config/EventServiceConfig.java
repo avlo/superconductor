3cbd71afafdd3d362c86c4da911d6f924f0e1c78
nick avlo <avlo@yahoo.com>
2025-06-30 12:27:23 -0700

    eventKindService now bean, ongoing refactor

--- autoconfigure/src/main/java/com/prosilion/superconductor/service/message/event/config/EventServiceConfig.java
+++ autoconfigure/src/main/java/com/prosilion/superconductor/service/message/event/config/EventServiceConfig.java
@@ -1,11 +1,14 @@
-package com.prosilion.superconductor.service.message.req.config;
+package com.prosilion.superconductor.service.message.event.config;
 
 import com.prosilion.nostr.enums.Kind;
+import com.prosilion.superconductor.service.event.service.EventKindService;
+import com.prosilion.superconductor.service.event.service.EventKindServiceIF;
 import com.prosilion.superconductor.service.event.service.plugin.EventKindPluginIF;
 import com.prosilion.superconductor.service.event.type.CanonicalEventKindPlugin;
 import com.prosilion.superconductor.service.event.type.EventKindPlugin;
 import com.prosilion.superconductor.service.event.type.EventPluginIF;
 import com.prosilion.superconductor.service.request.NotifierService;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -13,7 +16,12 @@ import org.springframework.lang.NonNull;
 
 @Slf4j
 @AutoConfiguration
-public class EventConfig {
+public class EventServiceConfig {
+
+  @Bean
+  EventKindServiceIF eventKindService(@NonNull List<EventKindPluginIF<Kind>> eventKindPlugins) {
+    return new EventKindService(eventKindPlugins);
+  }
 
   @Bean
 //  @ConditionalOnMissingBean
--- lib/src/main/java/com/prosilion/superconductor/service/event/service/EventKindService.java
+++ lib/src/main/java/com/prosilion/superconductor/service/event/service/EventKindService.java
@@ -3,25 +3,22 @@ package com.prosilion.superconductor.service.event.service;
 import com.prosilion.nostr.enums.Kind;
 import com.prosilion.nostr.event.GenericEventKindIF;
 import com.prosilion.superconductor.service.event.service.plugin.EventKindPluginIF;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
-import org.springframework.stereotype.Service;
 
-@Service
+@Slf4j
 public class EventKindService implements EventKindServiceIF {
   private final Map<Kind, EventKindPluginIF<Kind>> eventKindPluginsMap;
 
-  @Autowired
   public EventKindService(List<EventKindPluginIF<Kind>> eventKindPlugins) {
     this.eventKindPluginsMap = eventKindPlugins.stream().collect(
         Collectors.toMap(EventKindPluginIF::getKind, Function.identity()));
+    log.info("eventKindPluginsMap: {}", eventKindPluginsMap);
   }
 
   @Override

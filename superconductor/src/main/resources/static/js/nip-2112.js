66e1c10672326d7c838702b8582492ad7a4e148a
nick avlo <avlo@yahoo.com>
2025-06-29 21:53:23 -0700

    ui updates

--- superconductor/src/main/resources/static/js/nip-2112.js
+++ superconductor/src/main/resources/static/js/nip-2112.js
@@ -3,18 +3,23 @@ $(function () {
 });
 
 function generate2112TypeScriptEvent() {
+    let voteSelection = $("#2112-content").val();
+    
     const tags = [
-        ['v', $("#2112-v_tag").val()]
+        ['a', '30009:2bed79f81439ff794cf5ac5f7bff9121e257f399829e472c7a14d3e86fe76984:' + voteSelection],
     ];
+    
+    let voteIntegerAsString = voteSelection === 'UPVOTE' ? '1' : '-1'; 
 
     let event = {
         id: '',
         kind: Number($("#2112-kind").val()),
         created_at: Math.floor(Date.now() / 1000),
-        content: $("#2112-content").val(),
+        content: voteIntegerAsString,
         tags: tags,
         pubkey: '',
         sig: ''
     }
+    
     return event;
 }

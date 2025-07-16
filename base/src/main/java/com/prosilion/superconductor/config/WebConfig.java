162b97840b12c4a6b339f4fd5bfbd947505431a8
avlo <avlo@yahoo.com>
2025-07-16 00:46:58 -0700

    bean pickup

--- base/src/main/java/com/prosilion/superconductor/config/WebConfig.java
+++ base/src/main/java/com/prosilion/superconductor/config/WebConfig.java
@@ -4,6 +4,10 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @AutoConfiguration
-@ComponentScan(basePackageClasses = com.prosilion.superconductor.base.controller.NostrEventController.class)
+@ComponentScan(
+    basePackageClasses = com.prosilion.superconductor.base.controller.NostrEventController.class,
+    basePackages = {
+        "com.prosilion.superconductor.base.plugin.filter"
+    })
 public class WebConfig {
 }

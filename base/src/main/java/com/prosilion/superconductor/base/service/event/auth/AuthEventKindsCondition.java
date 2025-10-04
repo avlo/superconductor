5b0f0231b12653ba51439d414878efb752e0915a
nick avlo <avlo@yahoo.com>
2025-10-04 22:41:28 +0800

    tighten condition

--- base/src/main/java/com/prosilion/superconductor/base/service/event/auth/AuthEventKindsCondition.java
+++ base/src/main/java/com/prosilion/superconductor/base/service/event/auth/AuthEventKindsCondition.java
@@ -1,30 +1,21 @@
 package com.prosilion.superconductor.base.service.event.auth;
 
 import java.util.Arrays;
-import java.util.List;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class AuthEventKindsCondition implements Condition {
-
   @Override
   public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-    String kinds = context.getEnvironment()
+    String kinds = context
+        .getEnvironment()
         .getProperty("superconductor.auth.event.kinds", String.class, "");
 
     if (kinds.isEmpty()) {
       return false;
     }
 
-    String[] split = kinds.split(",");
-
-    List<String> splits = Arrays.stream(split).toList();
-
-    if (splits.isEmpty()) {
-      return false;
-    }
-
-    return true;
+    return !Arrays.stream(kinds.split(",")).toList().isEmpty();
   }
 }
--- base/src/main/java/com/prosilion/superconductor/base/service/event/auth/NoAuthEventKindsCondition.java
+++ base/src/main/java/com/prosilion/superconductor/base/service/event/auth/NoAuthEventKindsCondition.java
@@ -5,16 +5,11 @@ import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class NoAuthEventKindsCondition implements Condition {
-
   @Override
   public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-    String kinds = context.getEnvironment()
-        .getProperty("superconductor.auth.event.kinds", String.class, "");
-
-    if (kinds.isEmpty()) {
-      return true;
-    }
-
-    return false;
+    return context
+        .getEnvironment()
+        .getProperty("superconductor.auth.event.kinds", String.class, "")
+        .isEmpty();
   }
 }

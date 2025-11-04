2cac8e3ad15b48cea7d3094afda9e8c171937d68
nick avlo <avlo@yahoo.com>
2025-11-04 13:18:34 +0800

    readd modules, dirtiescontext for an IT

--- superconductor/h2db/src/test/java/com/prosilion/superconductor/h2db/service/JpaCacheServiceIT.java
+++ superconductor/h2db/src/test/java/com/prosilion/superconductor/h2db/service/JpaCacheServiceIT.java
@@ -22,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -31,6 +32,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Slf4j
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 public class JpaCacheServiceIT {
   private static final Identity IDENTITY = Factory.createNewIdentity();
   private static final PubKeyTag P_TAG = Factory.createPubKeyTag(IDENTITY);

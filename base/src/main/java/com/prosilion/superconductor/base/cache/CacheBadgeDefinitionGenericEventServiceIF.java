2681fb76154649ae40c3f0658de52b6b3ed8b76a
nick avlo <avlo@yahoo.com>
2026-05-27 14:21:35 -0700

    api up

--- base/src/main/java/com/prosilion/superconductor/base/cache/CacheBadgeDefinitionGenericEventServiceIF.java
+++ base/src/main/java/com/prosilion/superconductor/base/cache/CacheBadgeDefinitionGenericEventServiceIF.java
@@ -1,10 +1,14 @@
 package com.prosilion.superconductor.base.cache;
 
 import com.prosilion.nostr.event.BadgeDefinitionGenericEvent;
-import com.prosilion.nostr.event.EventIF;
-import org.springframework.lang.NonNull;
+import com.prosilion.superconductor.base.cache.mapped.CacheAddressableEventServiceIF;
 
-public interface CacheBadgeDefinitionGenericEventServiceIF extends CacheBadgeDefinitionBaseEventServiceIF<BadgeDefinitionGenericEvent> {
-  @Override
-  BadgeDefinitionGenericEvent materialize(@NonNull EventIF eventIF);
+/**
+ * This interface exists as shorthand convenience for developers via short/easily understandable:
+ *    CacheBadgeDefinitionGenericEventServiceIF variableName;
+ *
+ * rather than longer & more complex/error-prone variant:
+ *    CacheAddressableEventServiceIF<BadgeDefinitionGenericEvent> variableName;
+ */
+public interface CacheBadgeDefinitionGenericEventServiceIF extends CacheAddressableEventServiceIF<BadgeDefinitionGenericEvent> {
 }

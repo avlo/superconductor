99c35409a44c4db737ad930f2d9520f833ad3025
nick avlo <avlo@yahoo.com>
2026-06-13 20:19:19 -0700

    ui up

--- base/src/main/resources/static/js/generic-event.js
+++ base/src/main/resources/static/js/generic-event.js
@@ -5,11 +5,11 @@ const TAG_DEFINITIONS = {
     {field: "kind", type: "number"},
     {field: "publicKey", type: "text"},
     {field: "uuid", type: "text"},
-    {field: "relayUrl", type: "text"}
+    {field: "relayUrl", type: "text", default: "ws://localhost:5555"}
   ],
   p: [
     {field: "publicKey", type: "text"},
-    {field: "relayUrl", type: "text"}
+    {field: "relayUrl", type: "text", default: "ws://localhost:5555"}
   ],
   d: [
     {field: "uuid", type: "text"}
@@ -18,36 +18,45 @@ const TAG_DEFINITIONS = {
 
 $(function () {
   $("#send-generic").click(() => createEvent(gatherUnsignedEventContent()));
-  $("#add-tag").click(() => addTagRow());
+  addTagRow();
 });
 
-function addTagRow() {
+function addTagRow($afterRow) {
   const rowId = "tag-row-" + (tagRowCount++);
   const options = Object.keys(TAG_DEFINITIONS)
       .map((code) => '<option value="' + code + '">' + code + '</option>')
       .join("");
-  $("#tag-rows").append(
+  const $row = $(
     '<tr id="' + rowId + '">' +
       '<td><select class="form-control tag-key">' + options + '</select></td>' +
       '<td class="tag-values"></td>' +
-      '<td><button type="button" class="btn btn-default btn-sm" ' +
-          'onclick="$(\'#' + rowId + '\').remove()">Remove</button></td>' +
+      '<td>' +
+        '<button type="button" class="btn btn-light-green btn-sm add-tag-row">Add Tag</button> ' +
+        '<button type="button" class="btn btn-light-red btn-sm remove-tag-row">Remove Tag</button>' +
+      '</td>' +
     '</tr>'
   );
 
-  const $row = $("#" + rowId);
+  if ($afterRow && $afterRow.length) {
+    $afterRow.after($row);
+  } else {
+    $("#tag-rows").append($row);
+  }
+
   $row.find(".tag-key").on("change", function () {
     renderTagValueInputs($row, $(this).val());
   });
+  $row.find(".add-tag-row").on("click", () => addTagRow($row));
+  $row.find(".remove-tag-row").on("click", () => $row.remove());
   renderTagValueInputs($row, $row.find(".tag-key").val());
 }
 
 function renderTagValueInputs($row, code) {
   const $cell = $row.find(".tag-values");
   $cell.empty();
-  (TAG_DEFINITIONS[code] || []).forEach(({field, type}) =>
+  (TAG_DEFINITIONS[code] || []).forEach(({field, type, default: defaultValue}) =>
     $cell.append(
-      '<input type="' + type + '" class="form-control tag-value" data-field="' + field + '" placeholder="' + field + '">'
+      '<input type="' + type + '" class="form-control tag-value" data-field="' + field + '" placeholder="' + field + '" value="' + (defaultValue || "") + '">'
     )
   );
 }

e050aca207d0698c824a45ff689a2e33822a2989
nick avlo <avlo@yahoo.com>
2026-07-03 15:20:10 -0700

    rmd aux files

--- base/src/main/java/com/prosilion/superconductor/base/cache/CacheBadgeAwardGenericEventAuxServiceBaseIF.java
+++
@@ -1,8 +0,0 @@
-package com.prosilion.superconductor.base.cache;
-
-import com.prosilion.nostr.tag.AddressTag;
-import com.prosilion.nostr.tag.SetsPairedEventTagIF;
-import com.prosilion.superconductor.base.cache.mapped.CacheTagMappedEventAuxServiceIF;
-
-public interface CacheBadgeAwardGenericEventAuxServiceBaseIF<S extends SetsPairedEventTagIF, T extends SetsPairedEventTagIF> extends CacheTagMappedEventAuxServiceIF<T, AddressTag> {
-}

f2cd0a0cdf09ea3031634b72024b84f19c1dfa30
nick avlo <avlo@yahoo.com>
2026-07-26 22:07:20 -0700

    tests up

--- superconductor/redis/src/test/java/com/prosilion/superconductor/redis/service/event/CacheBadgeAwardGenericEventServiceIT.java
+++ superconductor/redis/src/test/java/com/prosilion/superconductor/redis/service/event/CacheBadgeAwardGenericEventServiceIT.java
@@ -1,83 +1,90 @@
-//package com.prosilion.superconductor.redis.service.event;
-//
-//import com.prosilion.nostr.event.BadgeAwardGenericEvent;
-//import com.prosilion.nostr.event.BadgeDefinitionGenericEvent;
-//import com.prosilion.nostr.event.internal.Relay;
-//import com.prosilion.nostr.message.EventMessage;
-//import com.prosilion.nostr.tag.AddressTag;
-//import com.prosilion.nostr.tag.IdentifierTag;
-//import com.prosilion.nostr.user.Identity;
-//import com.prosilion.nostr.user.PublicKey;
-//import com.prosilion.superconductor.base.cache.CacheBadgeAwardGenericEventAuxServiceIF;
-//import com.prosilion.superconductor.base.cache.CacheServiceIF;
-//import com.prosilion.superconductor.base.cache.mapped.CacheTagMappedEventServiceIF;
-//import com.prosilion.superconductor.base.service.event.EventServiceIF;
-//import io.github.tobi.laa.spring.boot.embedded.redis.standalone.EmbeddedRedisStandalone;
-//import lombok.NonNull;
-//import lombok.extern.slf4j.Slf4j;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.ActiveProfiles;
-//
-//import static com.prosilion.superconductor.redis.config.DataLoaderRedisTestIF.TEST_UNIT_UPVOTE;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@Slf4j
-//@EmbeddedRedisStandalone
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-//@ActiveProfiles("test")
-//public class CacheBadgeAwardGenericEventServiceIT {
-//  public final IdentifierTag upvoteIdentifierTag = new IdentifierTag(TEST_UNIT_UPVOTE);
-//  public final Identity identity = Identity.generateRandomIdentity();
-//
-//  private final BadgeDefinitionGenericEvent awardUpvoteDefinitionEvent;
-//  private final CacheBadgeAwardGenericEventAuxServiceIF<BadgeDefinitionGenericEvent, BadgeAwardGenericEvent<BadgeDefinitionGenericEvent>> cacheBadgeAwardGenericEventServiceIF;
-//  private final EventServiceIF eventServiceIF;
-//  private final Relay relay;
-//
-//  @Autowired
-//  public CacheBadgeAwardGenericEventServiceIT(
-//     @Value("${superconductor.relay.url}") String relayUri,
-//     @NonNull CacheServiceIF cacheServiceIF,
-//     @NonNull @Qualifier("eventService") EventServiceIF eventServiceIF,
-//     @NonNull @Qualifier("cacheBadgeAwardGenericEventService") CacheTagMappedEventServiceIF<BadgeAwardGenericEvent<BadgeDefinitionGenericEvent>, AddressTag> cacheBadgeAwardGenericEventServiceIF) {
-//    this.eventServiceIF = eventServiceIF;
-//    this.cacheBadgeAwardGenericEventServiceIF = (CacheBadgeAwardGenericEventAuxServiceIF<BadgeDefinitionGenericEvent, BadgeAwardGenericEvent<BadgeDefinitionGenericEvent>>) cacheBadgeAwardGenericEventServiceIF;
-//    this.relay = new Relay(relayUri);
-//    awardUpvoteDefinitionEvent = new BadgeDefinitionGenericEvent(identity, upvoteIdentifierTag, relay);
-//    cacheServiceIF.save(awardUpvoteDefinitionEvent);
-//  }
-//
-//  @Test
-//  public void testValidateSaveMaterializedBadgeAwardGenericEventUpvote() {
-//    PublicKey upvotedUserPublicKey = Identity.generateRandomIdentity().getPublicKey();
-//    BadgeAwardGenericEvent<BadgeDefinitionGenericEvent> badgeAwardGenericVoteEvent = new BadgeAwardGenericEvent<>(
-//       identity,
-//       upvotedUserPublicKey,
-//       relay,
-//       awardUpvoteDefinitionEvent);
-//
-//    eventServiceIF.processIncomingEvent(new EventMessage(badgeAwardGenericVoteEvent), relay);
-//    BadgeAwardGenericEvent<BadgeDefinitionGenericEvent> dbMaterializedGenericAwardEvent =
-//       cacheBadgeAwardGenericEventServiceIF.materialize(badgeAwardGenericVoteEvent).get();
-//    assertEquals(badgeAwardGenericVoteEvent, dbMaterializedGenericAwardEvent);
-//  }
-//
-//  @Test
-//  public void testValidateGetEventSaveBadgeAwardGenericEventUpvote() {
-//    PublicKey upvotedUserPublicKey = Identity.generateRandomIdentity().getPublicKey();
-//    BadgeAwardGenericEvent<BadgeDefinitionGenericEvent> badgeAwardGenericVoteEvent = new BadgeAwardGenericEvent<>(
-//       identity,
-//       upvotedUserPublicKey,
-//       relay,
-//       awardUpvoteDefinitionEvent);
-//
-//    eventServiceIF.processIncomingEvent(new EventMessage(badgeAwardGenericVoteEvent), relay);
-//
-//    BadgeAwardGenericEvent<BadgeDefinitionGenericEvent> dbGetEventGenericAwardEvent = cacheBadgeAwardGenericEventServiceIF.getEvent(badgeAwardGenericVoteEvent.getId(), badgeAwardGenericVoteEvent.requireRelayTagUrl()).orElseThrow();
-//    assertEquals(badgeAwardGenericVoteEvent, dbGetEventGenericAwardEvent);
-//  }
-//}
+package com.prosilion.superconductor.redis.service.event;
+
+import com.prosilion.nostr.event.BadgeAwardGenericEvent;
+import com.prosilion.nostr.event.BadgeDefinitionGenericEvent;
+import com.prosilion.nostr.event.internal.Relay;
+import com.prosilion.nostr.message.EventMessage;
+import com.prosilion.nostr.user.Identity;
+import com.prosilion.superconductor.autoconfigure.base.service.event.award.CacheBadgeAwardGenericEventService;
+import com.prosilion.superconductor.base.BaseIntegrationTestFixtures;
+import com.prosilion.superconductor.base.cache.CacheServiceIF;
+import com.prosilion.superconductor.base.service.event.EventServiceIF;
+import io.github.tobi.laa.spring.boot.embedded.redis.standalone.EmbeddedRedisStandalone;
+import java.util.Optional;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+@EmbeddedRedisStandalone
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
+public class CacheBadgeAwardGenericEventServiceIT extends BaseIntegrationTestFixtures {
+  private final BadgeDefinitionGenericEvent badgeDefinitionUpvoteEvent;
+  private final CacheBadgeAwardGenericEventService cacheBadgeAwardGenericEventService;
+
+  private final EventServiceIF eventServiceIF;
+  private final Relay relay;
+
+  @Autowired
+  public CacheBadgeAwardGenericEventServiceIT(
+     @Value("${superconductor.relay.url}") String relayUri,
+     @NonNull Identity superconductorInstanceIdentity,
+     @NonNull CacheServiceIF cacheServiceIF,
+     @NonNull @Qualifier("eventService") EventServiceIF eventServiceIF,
+     @NonNull @Qualifier("cacheBadgeAwardGenericEventService") CacheBadgeAwardGenericEventService cacheBadgeAwardGenericEventService) {
+    super(superconductorInstanceIdentity);
+    this.eventServiceIF = eventServiceIF;
+    this.cacheBadgeAwardGenericEventService = cacheBadgeAwardGenericEventService;
+    this.relay = new Relay(relayUri);
+
+    this.badgeDefinitionUpvoteEvent = new BadgeDefinitionGenericEvent(
+       upvoteDefnCreator,
+       upvoteIdentifierTag,
+       relay);
+    cacheServiceIF.save(badgeDefinitionUpvoteEvent);
+  }
+
+  @Test
+  public void testSaveBadgeAwardGenericEventUpvote() {
+    BadgeAwardGenericEvent<BadgeDefinitionGenericEvent> badgeAwardGenericEvent =
+       new BadgeAwardGenericEvent<>(
+          parameterAimgIdentity,
+          recipient.getPublicKey(),
+          badgeDefinitionUpvoteEvent,
+          relay);
+
+    eventServiceIF.processIncomingEvent(new EventMessage(badgeAwardGenericEvent), relay);
+    BadgeAwardGenericEvent<BadgeDefinitionGenericEvent> dbGenericAwardEvent =
+       cacheBadgeAwardGenericEventService
+          .materialize(badgeAwardGenericEvent.asGenericEventRecord())
+          .orElseThrow();
+
+    assertEquals(badgeDefinitionUpvoteEvent, dbGenericAwardEvent.getBadgeDefinitionEvent());
+  }
+
+  @Test
+  public void testGetByEventId() {
+    BadgeAwardGenericEvent<BadgeDefinitionGenericEvent> badgeAwardGenericEvent =
+       new BadgeAwardGenericEvent<>(
+          parameterAimgIdentity,
+          recipient.getPublicKey(),
+          badgeDefinitionUpvoteEvent,
+          relay);
+
+    eventServiceIF.processIncomingEvent(new EventMessage(badgeAwardGenericEvent), relay);
+
+    Optional<BadgeAwardGenericEvent<BadgeDefinitionGenericEvent>> actualAwardUpvoteDefinitionEvent =
+       cacheBadgeAwardGenericEventService.getEvent(badgeAwardGenericEvent.getId(), relay);
+
+    assertTrue(actualAwardUpvoteDefinitionEvent.isPresent());
+  }
+}

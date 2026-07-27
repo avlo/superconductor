112689ee471574f987d86f1cae06b4bad6574714
nick avlo <avlo@yahoo.com>
2026-07-27 14:06:30 -0700

    tests up

---
+++ superconductor/redis/src/test/java/com/prosilion/superconductor/redis/BadgeAwardUpvoteEventRemoteSupplierMessageIT.java
@@ -0,0 +1,32 @@
+package com.prosilion.superconductor.redis;
+
+import com.prosilion.nostr.NostrException;
+import com.prosilion.nostr.user.Identity;
+import com.prosilion.superconductor.base.BaseBadgeAwardUpvoteEventRemoteSupplierMessageIT;
+import com.prosilion.superconductor.redis.config.SingleContainerTestConfig;
+import io.github.tobi.laa.spring.boot.embedded.redis.standalone.EmbeddedRedisStandalone;
+import java.io.IOException;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@Slf4j
+@EmbeddedRedisStandalone
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
+@Import(SingleContainerTestConfig.class)
+public class BadgeAwardUpvoteEventRemoteSupplierMessageIT extends BaseBadgeAwardUpvoteEventRemoteSupplierMessageIT {
+  @Autowired
+  BadgeAwardUpvoteEventRemoteSupplierMessageIT(
+     @NonNull @Value("${superconductor.relay.url}") String superconductorRelayUrl,
+     @NonNull @Value("${superconductor.relay.url.two}") String superconductorRelayUrlTwo,
+     @NonNull @Value("${superconductor.relay.url.three}") String superconductorRelayUrlThree,
+     @NonNull Identity superconductorInstanceIdentity) throws IOException, NostrException {
+    super(superconductorRelayUrl, superconductorRelayUrlTwo, superconductorRelayUrlThree, superconductorInstanceIdentity);
+  }
+}

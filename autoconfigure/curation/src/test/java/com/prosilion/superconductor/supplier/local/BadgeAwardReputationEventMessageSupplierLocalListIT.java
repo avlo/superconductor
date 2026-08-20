9b783b1afe199699d74d5473bd965b7d1a953c4d
nick avlo <avlo@yahoo.com>
2026-08-20 15:32:35 -0700

    tests up

--- autoconfigure/curation/src/test/java/com/prosilion/superconductor/supplier/local/BadgeAwardReputationEventMessageSupplierLocalListIT.java
+++ autoconfigure/curation/src/test/java/com/prosilion/superconductor/supplier/local/BadgeAwardReputationEventMessageSupplierLocalListIT.java
@@ -4,7 +4,6 @@ import com.prosilion.nostr.NostrException;
 import com.prosilion.nostr.enums.Kind;
 import com.prosilion.nostr.event.BadgeAwardGenericEvent;
 import com.prosilion.nostr.event.BadgeDefinitionGenericEvent;
-import com.prosilion.nostr.event.EventIF;
 import com.prosilion.nostr.event.internal.Relay;
 import com.prosilion.nostr.filter.Filters;
 import com.prosilion.nostr.filter.event.KindFilter;
@@ -16,7 +15,6 @@ import com.prosilion.superconductor.base.cache.CacheServiceIF;
 import com.prosilion.superconductor.supplier.local.abstracts.AbstractBadgeAwardReputationEventMessageSupplierLocalListIT;
 import com.prosilion.superconductor.util.EventAttributesMap;
 import io.github.tobi.laa.spring.boot.embedded.redis.standalone.EmbeddedRedisStandalone;
-import java.util.List;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -49,50 +47,25 @@ public class BadgeAwardReputationEventMessageSupplierLocalListIT extends Abstrac
 
   @Test
   void aSuperconductorEventThenAfterimageReq() throws NostrException {
-    BadgeAwardGenericEvent<BadgeDefinitionGenericEvent> upvoteEvent_1 = createUpvoteEvent(new Relay(definitionEventRelayUrl), recipient.getPublicKey());
+    createAndSubmitUpvoteEvent(recipient.getPublicKey(), "1");
+    createAndSubmitUpvoteEvent(recipientDifferent.getPublicKey(), "1");
+    createAndSubmitUpvoteEvent(recipient.getPublicKey(), "2");
+    createAndSubmitUpvoteEvent(recipient.getPublicKey(), "3");
+    createAndSubmitUpvoteEvent(recipientDifferent.getPublicKey(), "2");
+  }
 
+  private void createAndSubmitUpvoteEvent(PublicKey recipientPublicKey, String expectedScore) {
     submitSCEventWithDuration_backup(
-       upvoteEvent_1,
+       createUpvoteEvent(new Relay(definitionEventRelayUrl), recipientPublicKey),
        definitionEventRelayUrl,
        new Filters(
           new ReferencedPublicKeyFilter(
-             new PubKeyTag(recipient.getPublicKey())),
+             new PubKeyTag(recipientPublicKey)),
           new KindFilter(Kind.CURATION_SETS_BADGE_AWARD_EVENT)));
 
     assertEquals(
-       "1",
-       submitAfterImageReq(new PubKeyTag(recipient.getPublicKey()), awardEventRelayUrl).getFirst().getContent());
-
-//    below is different recipient, works fine
-//    PublicKey newRecipient = Identity.generateRandomIdentity().getPublicKey();
-//    BadgeAwardGenericEvent<BadgeDefinitionGenericEvent> upvoteEvent2 = createUpvoteEvent(new Relay(definitionEventRelayUrl), newRecipient);
-//
-//    submitSCEventWithDuration_backup(
-//       upvoteEvent2,
-//       definitionEventRelayUrl,
-//       new Filters(
-//          new ReferencedPublicKeyFilter(
-//             new PubKeyTag(newRecipient)),
-//          new KindFilter(Kind.CURATION_SETS_BADGE_AWARD_EVENT)));
-//
-//    assertEquals(
-//       "1",
-//       submitAfterImageReq(new PubKeyTag(newRecipient), awardEventRelayUrl).getFirst().getContent());
-
-//    below is same 1st recipient, fails    
-    BadgeAwardGenericEvent<BadgeDefinitionGenericEvent> upvoteEvent_3 = createUpvoteEvent(new Relay(definitionEventRelayUrl), recipient.getPublicKey());
-
-    List<EventIF> eventIFS = submitSCEventWithDuration_backup(
-       upvoteEvent_3,
-       definitionEventRelayUrl,
-       new Filters(
-          new ReferencedPublicKeyFilter(
-             new PubKeyTag(recipient.getPublicKey())),
-          new KindFilter(Kind.CURATION_SETS_BADGE_AWARD_EVENT)));
-
-    assertEquals(
-       "2",
-       submitAfterImageReq(new PubKeyTag(recipient.getPublicKey()), awardEventRelayUrl).getFirst().getContent());
+       expectedScore,
+       submitAfterImageReq(new PubKeyTag(recipientPublicKey), awardEventRelayUrl).getFirst().getContent());
   }
 
   protected BadgeAwardGenericEvent<BadgeDefinitionGenericEvent> createUpvoteEvent(Relay relay, PublicKey recipientPublicKey) {
